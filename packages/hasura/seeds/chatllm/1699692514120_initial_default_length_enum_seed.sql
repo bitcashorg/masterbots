baebd7dc9c6504dcbbdced726374ac9b1419ac31
Jun Dam <jun@100xinvestors.com>
2023-11-11 00:59:50 -0800

    hasura seed data

---
+++ packages/hasura/seeds/chatllm/1699692514120_initial_default_length_enum_seed.sql
@@ -0,0 +1,4 @@
+SET check_function_bodies = false;
+INSERT INTO public.default_length_enum (value) VALUES ('detailed');
+INSERT INTO public.default_length_enum (value) VALUES ('clear_and_succinct');
+INSERT INTO public.default_length_enum (value) VALUES ('concise');
