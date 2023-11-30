eb7132e57158a26bfb43c9f4f5b0044948a9ded7
Gabo Esquivel <contact@gaboesquivel.com>
2023-11-30 15:08:47 -0600

    db: update seeds

--- apps/hasura/seeds/masterbots/1699692478313_initial_default_complexity_enum_seed.sql
+++ apps/hasura/seeds/masterbots/1699692478313_initial_default_complexity_enum_seed.sql
@@ -1,4 +1,4 @@
 SET check_function_bodies = false;
-INSERT INTO public.default_complexity_enum (value) VALUES ('expert');
-INSERT INTO public.default_complexity_enum (value) VALUES ('adult');
-INSERT INTO public.default_complexity_enum (value) VALUES ('eli5');
+INSERT INTO public.complexity_enum (value) VALUES ('expert');
+INSERT INTO public.complexity_enum (value) VALUES ('adult');
+INSERT INTO public.complexity_enum (value) VALUES ('eli5');
--- apps/hasura/seeds/masterbots/1699692514120_initial_default_length_enum_seed.sql
+++ apps/hasura/seeds/masterbots/1699692514120_initial_default_length_enum_seed.sql
@@ -1,4 +1,4 @@
 SET check_function_bodies = false;
-INSERT INTO public.default_length_enum (value) VALUES ('detailed');
-INSERT INTO public.default_length_enum (value) VALUES ('clear_and_succinct');
-INSERT INTO public.default_length_enum (value) VALUES ('concise');
+INSERT INTO public.length_enum (value) VALUES ('detailed');
+INSERT INTO public.length_enum (value) VALUES ('clear_and_succinct');
+INSERT INTO public.length_enum (value) VALUES ('concise');
