9146582e63b9655ef5714515acbd1d95123e194f
Gabo Esquivel <contact@gaboesquivel.com>
2023-11-30 15:08:47 -0600

    db: update seeds

--- apps/hasura/seeds/masterbots/1699692554265_initial_default_tone_enum_seed.sql
+++ apps/hasura/seeds/masterbots/1699692554265_initial_default_tone_enum_seed.sql
@@ -1,4 +1,4 @@
 SET check_function_bodies = false;
-INSERT INTO public.default_tone_enum (value) VALUES ('professional');
-INSERT INTO public.default_tone_enum (value) VALUES ('neutral');
-INSERT INTO public.default_tone_enum (value) VALUES ('friendly');
+INSERT INTO public.tone_enum (value) VALUES ('professional');
+INSERT INTO public.tone_enum (value) VALUES ('neutral');
+INSERT INTO public.tone_enum (value) VALUES ('friendly');
--- apps/hasura/seeds/masterbots/1699692607971_initial_default_type_enum_seed.sql
+++ apps/hasura/seeds/masterbots/1699692607971_initial_default_type_enum_seed.sql
@@ -1,4 +1,4 @@
 SET check_function_bodies = false;
-INSERT INTO public.default_type_enum (value) VALUES ('narrative');
-INSERT INTO public.default_type_enum (value) VALUES ('step_by_step');
-INSERT INTO public.default_type_enum (value) VALUES ('bullet_points');
+INSERT INTO public.type_enum (value) VALUES ('narrative');
+INSERT INTO public.type_enum (value) VALUES ('step_by_step');
+INSERT INTO public.type_enum (value) VALUES ('bullet_points');
