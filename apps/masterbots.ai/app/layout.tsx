430d98f064f15afc6f1c49864905f6b956f92c11
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-05 16:34:52 -0600

    chore: update title and meta

--- apps/masterbots.ai/app/layout.tsx
+++ apps/masterbots.ai/app/layout.tsx
@@ -11,10 +11,10 @@ import { Header } from '@/components/header'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Masterbots',
+    template: `%s - Masterbots`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'Specialized AI chatbots',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
