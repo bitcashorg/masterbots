83505903c5f7991fb7b55f23fae8ba0cd60e8ec2
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-15 11:56:11 -0600

    feat: mb-genql package (#7)
    * wip: masterbots-genql package
    
    * wip: google auth
    
    * feat: mb-genql package

---
+++ packages/mb-genql/src/index.ts
@@ -0,0 +1,57 @@
+import { createClient } from '../generated'
+import { GraphqlOperation } from '@genql/runtime'
+import { endpoints, MbEnv } from 'mb-env'
+import { createClient as createWsClient } from 'graphql-ws'
+
+export * from '../generated'
+
+// Server side client
+export function createMbClient({ config, jwt, env, adminSecret }: GraphQLSdkProps = {}) {
+  const { subscribe } = createWsClient({
+    url: endpoints[env || 'prod'].replace('http', 'ws'),
+  })
+
+  const client = createClient({
+    fetcher: async (operation: any) => {
+      const headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        ...(jwt ? { Authorization: `Bearer ${jwt}` } : {}),
+        ...(adminSecret ? { 'x-hasura-admin-secret': adminSecret } : {}),
+      }
+
+      console.log(
+        '\n ==> GraphQL Query : \n',
+        JSON.stringify((operation as GraphqlOperation).query.replaceAll('"', ''))
+      )
+
+      let fetchResponse
+      try {
+        fetchResponse = fetch(endpoints[env || 'prod'], {
+          method: 'POST',
+          headers,
+          body: JSON.stringify(operation),
+          ...config,
+        }).then((response) => response.json())
+      } catch (error) {
+        console.error('Error in graphql fetcher', error)
+      }
+
+      return fetchResponse
+    },
+  })
+
+  return {
+    subscribe,
+    ...client,
+  }
+}
+
+export type MbClient = ReturnType<typeof createMbClient>
+
+type GraphQLSdkProps = {
+  config?: RequestInit
+  jwt?: string
+  env?: MbEnv
+  adminSecret?: string
+}
