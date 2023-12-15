9a20332648b1413b33fb2c291a4e20b4498145ee
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-15 15:42:39 -0600

    feat: restore masterbots logo

--- apps/masterbots.ai/app/sign-in/page.tsx
+++ apps/masterbots.ai/app/sign-in/page.tsx
@@ -1,14 +1,30 @@
 import { auth } from '@/auth'
 import { LoginButton } from '@/components/login-button'
 import { redirect } from 'next/navigation'
+import Image from 'next/image'
 
 export default async function SignInPage() {
   const session = await auth()
   // redirect to home if user is already logged in
   if (session?.user) redirect('/')
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <LoginButton />
+    <div className="flex flex-col h-[calc(100vh-theme(spacing.16))] items-center py-10">
+      <div
+        className="relative flex justify-center w-full max-w-[726px] py-4 text-center "
+        style={{ height: 200 }}
+      >
+        <Image
+          src="/images/masterbotslogo.png"
+          fill={true}
+          alt="Masterbots Logo"
+          style={{ maxWidth: 726 }}
+          className="flex self-center"
+        />
+      </div>
+
+      <div className="flex justify-center pt-10">
+        <LoginButton />
+      </div>
     </div>
   )
 }
