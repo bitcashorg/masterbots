d23bd4721d7999f9cd3930e18d2dc47e4d82cc90
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-15 10:58:30 -0600

    feat: google auth, mb packages, hasura jwt wip (#8)
    * wip: google auth
    
    * wip: jwt generation on vercel edge

---
+++ packages/mb-lib/src/hasura/hasura.lib.ts
@@ -0,0 +1,9 @@
+import { HasuraClaims } from 'mb-types'
+
+export function generateHasuraClaims({ account, role }: { account: string; role: string }) {
+  return {
+    'x-hasura-allowed-roles': [...role.split(','), 'anonymous'],
+    'x-hasura-default-role': 'user',
+    'x-hasura-user-id': account,
+  } as HasuraClaims
+}
