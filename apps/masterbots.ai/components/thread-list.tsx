2be0d45183bee5bb40482d083af4837cadf54e31
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-19 08:18:07 -0600

    wip: chat

--- apps/masterbots.ai/components/thread-list.tsx
+++ apps/masterbots.ai/components/thread-list.tsx
@@ -9,12 +9,6 @@ import { Thread } from 'mb-genql'
 export default function ThreadList({ threads }: { threads: Thread[] }) {
   return (
     <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value={`item-new-chat`} key={'new-chat'}>
-        <AccordionTrigger>Start a new chat with HealthBot</AccordionTrigger>
-        <AccordionContent>
-          Type something on input box at the boom to start a new chat
-        </AccordionContent>
-      </AccordionItem>
       {threads.map((thread, key) => (
         <AccordionItem value={`item-${key}`} key={key}>
           <AccordionTrigger>
