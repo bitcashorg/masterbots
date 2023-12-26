a512fe874c3cbb6225091e764386a221edf55850
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-26 17:19:51 -0600

    db: update seeds

--- apps/hasura/seeds/masterbots/1699692478311_initial_message_type_enum_seed.sql
+++ apps/hasura/seeds/masterbots/1699692478311_initial_message_type_enum_seed.sql
@@ -1,3 +1,11 @@
+
 SET check_function_bodies = false;
-INSERT INTO public.message_type_enum (value) VALUES ('user');
-INSERT INTO public.message_type_enum (value) VALUES ('chatbot');
+
+
+INSERT INTO public.message_type_enum (value)
+VALUES ('user');
+
+
+INSERT INTO public.message_type_enum (value)
+VALUES ('assistant');
+
