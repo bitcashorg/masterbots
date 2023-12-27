27392841156c2bac338fa38d5836cf06b3b25c56
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-26 18:14:30 -0600

    wip: system and user prompts in chat

--- apps/masterbots.ai/app/(chat)/[chatbot]/[threadId]/page.tsx
+++ apps/masterbots.ai/app/(chat)/[chatbot]/[threadId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from '@/auth'
 import { Chat } from '@/components/chat'
 import { getThread } from '@/services/db'
 import { nanoid } from '@/lib/utils'
+import { Message } from 'ai/react'
 
 export default async function ChatPage({ params }: ChatPageProps) {
   const session = await auth()
@@ -13,7 +14,38 @@ export default async function ChatPage({ params }: ChatPageProps) {
 
   if (!session) redirect(`/sign-in?next=/${params.threadId}/${params.threadId}`)
 
-  return <Chat id={nanoid()} initialMessages={[]} bot={params.chatbot} />
+  // format all chatbot prompts as chatgpt 'system' messages
+  const chatbotSystemPrompts: Message[] = thread.chatbot.prompts.map(
+    ({ prompt }) => ({
+      id: prompt.promptId.toString(),
+      role: 'system',
+      content: prompt.content,
+      createdAt: new Date()
+    })
+  )
+
+  // format all user prompts and chatgpt 'assistant' messages
+  const userAndAssistantMessages: Message[] = thread.messages.map(m => ({
+    id: m.messageId,
+    role: m.role as Message['role'],
+    content: m.content,
+    createdAt: m.createdAt
+  }))
+
+  // concatenate all message to pass it to chat component
+  const initialMessages: Message[] = chatbotSystemPrompts.concat(
+    userAndAssistantMessages
+  )
+
+  // we always start a new openai chat that why id=nanoid()
+  // and we pass our system prompts along with assistant and user messages from our db.
+  return (
+    <Chat
+      id={nanoid()}
+      initialMessages={initialMessages}
+      bot={params.chatbot}
+    />
+  )
 }
 
 export interface ChatPageProps {
