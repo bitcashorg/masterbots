6a5e38f4fbcb29281d2457f474f33404fa56ed3c
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-27 15:51:22 -0600

    wip: prompting strategy

--- apps/masterbots.ai/app/api/chat/route.ts
+++ apps/masterbots.ai/app/api/chat/route.ts
@@ -48,7 +48,7 @@ export async function POST(req: Request) {
           ...messages,
           {
             content: completion,
-            role: 'system'
+            role: 'assistant'
           }
         ]
       }
