4616a038f1d8ca6f610aa97614a67efb25148aca
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-26 18:36:35 -0600

    wip: chat functionality

--- apps/masterbots.ai/app/(chat)/page.tsx
+++ apps/masterbots.ai/app/(chat)/page.tsx
@@ -1,11 +1,5 @@
-import MbChat from '@/components/mb-chat'
 import ThreadPanel from '@/components/thread-panel'
 
 export default function IndexPage() {
-  return (
-    <div>
-      <ThreadPanel />
-      <MbChat bot={'HealthBot'} />
-    </div>
-  )
+  return <ThreadPanel />
 }
