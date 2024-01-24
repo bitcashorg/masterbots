d7615e89ee3c16248a27a4210cde926536b934dc
TopETH <supernathanliu@gmail.com>
2024-01-24 16:27:55 -0500

    fix: upt real categories

--- apps/masterbots.ai/components/sidebar-link.tsx
+++ apps/masterbots.ai/components/sidebar-link.tsx
@@ -3,81 +3,73 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
-import { Chatbot } from 'mb-genql'
+import { Category, Chatbot } from 'mb-genql'
 import { useParams } from 'next/navigation'
 import { IconCaretRight } from './ui/icons'
 import { motion } from "framer-motion"
+import { categoryAvatars } from '@/lib/categoris-avatars'
 
-export default function SidebarLink({ bot }: { bot: Chatbot }) {
+export default function SidebarLink({ category }: { category: Category }) {
   const { chatbot, threadId } = useParams<{ chatbot: string, threadId?: string }>()
   const [isCollapsed, setIsCollapsed] = React.useState(false)
-  const [activeThreadId, setActiveThreadId] = React.useState<number | null>(null)
-
-  const threads = [
-    { id: 1, name: 'healthBot1', avatar: 'https://robohash.org/JHS.png?set=set4' },
-    { id: 2, name: 'healthBot2', avatar: 'https://robohash.org/JHS.png?set=set4' },
-    { id: 3, name: 'healthBot3', avatar: 'https://robohash.org/JHS.png?set=set4' }
-  ]
+  const [activeChatbot, setActiveChatbot] = React.useState<Chatbot | null>(null)
 
   React.useEffect(() => {
     setIsCollapsed(false)
-    if (chatbot !== bot.name.toLowerCase()) {
-      setActiveThreadId(null)
+    if (category.chatbots.length && category.chatbots.filter(c => c.chatbot.name.toLowerCase().trim() === chatbot?.trim()).length) {
+      setActiveChatbot(category.chatbots.filter(c => c.chatbot.name.toLowerCase().trim() === chatbot?.trim())[0].chatbot)
     } else {
-      setActiveThreadId(Number(threadId))
+      setActiveChatbot(null)
     }
-  } , [bot, chatbot, threadId])
+  } , [category, chatbot, threadId])
 
-  const handleClickMainBot = () => {
-    if (activeThreadId) {
-      setActiveThreadId(null)
-    } else if (chatbot === bot.name.toLowerCase()) {
+  const handleClickCategory = () => {
+    if (!activeChatbot) {
       setIsCollapsed(!isCollapsed) 
     }
   }
-
   return (
     <div className={`flex flex-col ${isCollapsed ? 'border-b-[1px] border-[#1E293B]' : ''}`}>
-      <div className={cn('flex', bot?.name.toLowerCase().trim() === chatbot?.trim() && 'bg-[#1E293B]', activeThreadId && 'justify-center')}>
+      <div className={cn('flex', isCollapsed && 'bg-[#1E293B]', activeChatbot && 'justify-center')}>
         <Link
-          href={`/${bot.name.toLowerCase()}`}
+          href='/'
           className={cn(
             'flex items-center pr-5 py-3 cursor-pointer relative origin-left transition-all ease-in-out duration-300',
-            activeThreadId ? 'text-xs opacity-50' : 'grow'
+            activeChatbot ? 'text-xs opacity-50' : 'grow pl-5'
           )}
-          onClick={handleClickMainBot}
-          shallow={true}
+          onClick={handleClickCategory}
         >
           <motion.div
           className='overflow-hidden'
           animate={{
-            width: activeThreadId ? '0' : 'auto',
+            width: activeChatbot ? '0' : 'auto',
           }}
           >
-            <Image
-              src={bot.avatar || '/path/to/default/avatar.png'}
-              alt={bot.name}
+           {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={categoryAvatars.get(category.name) || '/path/to/default/avatar.png'}
+              alt={category.name}
               width={50} // replace with your desired width
               height={50} // replace with your desired height
               className="object-cover rounded-full"
             />
           </motion.div>
-          <span className="pl-3">{bot.name}</span>
+          <span className="pl-3">{category.name}</span>
           <IconCaretRight className={`transition duration-300 ease-in-out
           absolute
-          stroke-[#09090b] dark:stroke-[#FAFAFA] ${isCollapsed ? 'rotate-90 right-5' : activeThreadId ? 'rotate-180 right-0 scale-75' : 'right-5'}`} />
+          stroke-[#09090b] dark:stroke-[#FAFAFA] ${isCollapsed ? 'rotate-90 right-5 xl:right-5 lg:right-2' : activeChatbot ? 'rotate-180 right-0 scale-75' : 'right-5 xl:right-5 lg:right-2'}`} />
         </Link>
-        { activeThreadId ? <div
+        { activeChatbot ? <div
           className='flex items-center pl-2 py-3'
         >
           <Image
-            src={threads.filter(i => i.id === activeThreadId)[0].avatar || '/path/to/default/avatar.png'}
-            alt={bot.name}
+            src={activeChatbot.avatar || '/path/to/default/avatar.png'}
+            alt={category.name}
             width={32}
             height={32}
             className="object-cover rounded-full"
           />
-          <span className="pl-3">{threads.filter(i => i.id === activeThreadId)[0].name}</span>
+          <span className="pl-3">{activeChatbot.name}</span>
         </div> : '' }
       </div>
       {
@@ -86,28 +78,25 @@ export default function SidebarLink({ bot }: { bot: Chatbot }) {
           ml-5 flex-col border-l-[1px] border-[#1E293B]'
           initial={{height: 0}}
           animate={{
-            height: isCollapsed && threads?.length ? "" : "0px",
+            height: isCollapsed && category.chatbots.length ? "" : "0px",
           }}
         >
           {
-            threads?.length && threads.map((thread) => 
-              // <Link href={`/${bot.name.toLowerCase()}/${thread?.id}`} className={cn(
-              <div
-              onClick={() => { setActiveThreadId(thread.id); setIsCollapsed(false) }}
+            category.chatbots.map((chatbot) => 
+            <Link href={`/${chatbot.chatbot.name.toLowerCase()}`}
               className={cn(
                 'flex items-center px-[20px] py-[12px]',
-                ( thread?.id === activeThreadId ) && 'dark:bg-slate-800 dark-slate-400'
-              )} key={thread.id}>
+                chatbot.chatbot.chatbotId === activeChatbot?.chatbotId && 'dark:bg-slate-800 dark-slate-400'
+              )} key={chatbot.chatbotId}>
                 <Image
-                  src={thread.avatar}
-                  alt={thread.name}
+                  src={chatbot.chatbot.avatar || '/path/to/default/avatar.png'}
+                  alt={chatbot.chatbot.name}
                   width={30}
                   height={30}
                   className="object-cover rounded-full"
                 />
-                <span className="pl-3">{thread.name}</span>
-              </div>
-              // </Link>
+                <span className="pl-3">{chatbot.chatbot.name}</span>
+              </Link>
             )
           }
         </motion.div>
