d68a4433807491d489016374796c5e20e21721f0
TopETH <supernathanliu@gmail.com>
2024-01-24 12:45:19 -0500

    impr: responsive sidebar

--- apps/masterbots.ai/app/(chat)/layout.tsx
+++ apps/masterbots.ai/app/(chat)/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarDesktop } from '@/components/sidebar-desktop'
+import { ResponsiveSidebar } from '@/components/sidebar-responsive'
 
 interface ChatLayoutProps {
   children: React.ReactNode
@@ -8,8 +8,8 @@ export default async function ChatLayout({ children }: ChatLayoutProps) {
   return (
     <div className="relative flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
       {/* @ts-ignore */}
-      <SidebarDesktop />
-      <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
+      <ResponsiveSidebar />
+      <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out lg:pl-[250px] xl:pl-[300px]">
         {children}
       </div>
     </div>
