71fe0f72119d39d758160744e018863b284fd38f
TopETH <supernathanliu@gmail.com>
2024-01-28 20:11:44 -0500

    chore: remove ts-ignore mark

--- apps/masterbots.ai/app/(chat)/layout.tsx
+++ apps/masterbots.ai/app/(chat)/layout.tsx
@@ -7,7 +7,6 @@ interface ChatLayoutProps {
 export default async function ChatLayout({ children }: ChatLayoutProps) {
   return (
     <div className="relative flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
-      {/* @ts-ignore */}
       <ResponsiveSidebar />
       <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out lg:pl-[250px] xl:pl-[300px]">
         {children}
