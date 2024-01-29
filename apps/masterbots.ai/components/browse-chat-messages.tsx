d93c229e2d6bff42785f3271b7ff62d32446c361
TopETH <supernathanliu@gmail.com>
2024-01-29 10:24:05 -0500

    fix: upt thread view UI of browse

--- apps/masterbots.ai/components/browse-chat-messages.tsx
+++ apps/masterbots.ai/components/browse-chat-messages.tsx
@@ -8,13 +8,20 @@ import {
   AccordionTrigger
 } from '@/components/ui/accordion'
 import { cn, createMessagePairs } from '@/lib/utils'
-import { IconUser } from '@/components/ui/icons'
-import { ChatMessageActions } from '@/components/chat-message-actions'
+import {
+  IconChatMessage,
+  IconDownVote,
+  IconShare,
+  IconUpVote,
+  IconUser
+} from '@/components/ui/icons'
 import React from 'react'
 import * as AI from 'ai'
-import { Message, User } from 'mb-genql'
+import { Chatbot, Message, User } from 'mb-genql'
 import Image from 'next/image'
-import { ChatMessage } from './chat-message'
+import { BrowseChatMessage } from './browse-chat-message'
+import { Separator } from './ui/separator'
+import Link from 'next/link'
 
 type MessagePair = {
   userMessage: Message
@@ -32,10 +39,12 @@ function convertMessage(message: Message) {
 
 export function BrowseChatMessages({
   messages,
-  user
+  user,
+  chatbot
 }: {
   messages: Message[]
   user?: User
+  chatbot?: Chatbot
 }) {
   const [pairs, setPairs] = React.useState<MessagePair[]>([])
 
@@ -48,48 +57,119 @@ export function BrowseChatMessages({
 
   return (
     <div className="w-full">
-      <Accordion type="multiple">
-        {pairs.map((pair: MessagePair, key) => (
-          <AccordionItem key={key} value={pair.userMessage.messageId}>
-            <AccordionTrigger className="hover:no-underline">
-              {user?.profilePicture ? (
-                <Image
-                  className="w-6 h-6 transition-opacity duration-300 rounded-full select-none ring-1 ring-zinc-100/10 hover:opacity-80"
-                  src={user?.profilePicture || ''}
-                  alt={user?.username ?? 'Avatar'}
-                  height={48}
-                  width={48}
-                />
-              ) : (
-                <div
-                  className={cn(
-                    'flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow',
-                    'bg-background'
-                  )}
-                >
-                  <IconUser />
+      <div
+        className="relative bg-cover py-10"
+        style={{ backgroundImage: 'url("/browse-background.svg")' }}
+      >
+        <div
+          className="dark:bg-[#09090B] bg-white rounded-lg p-6 md:w-[600px]
+          flex flex-row gap-3 relative mx-auto"
+        >
+          <div className="w-2/3 flex flex-col gap-3">
+            <div className="text-2xl font-black">{chatbot?.name}</div>
+            <Separator className="bg-[#1E293B]" />
+            <div className="text-xl font-semibold">
+              {chatbot?.categories[0].category.name}
+            </div>
+            <div className="text-base">
+              <div className="font-medium">
+                {chatbot?.description ? <div>{chatbot?.description}</div> : ''}
+              </div>
+              <div className="font-light">
+                Threads:{' '}
+                <span className="dark:text-[#71717A]">
+                  {chatbot?.threads.length ?? 1}
+                </span>
+                <div>
+                  Views: <span className="dark:text-[#71717A]">230</span>
                 </div>
-              )}
-              <div className="flex-1 px-1 ml-4 space-y-2 text-left text-[1.25rem] ">
-                {pair.userMessage.content}
-                {/* <span className="opacity-30 ml-4 font-normal">
+                <div>
+                  Read time: <span className="dark:text-[#71717A]">7 min</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3 relative flex items-end">
+            <div className="flex flex-col text-xs items-center w-full gap-2">
+              <Link
+                style={{ wordSpacing: '4px' }}
+                className="text-[#388DE2]"
+                href={`/${chatbot?.name.toLowerCase()}`}
+              >
+                Go to {chatbot?.name} chat &gt;
+              </Link>
+              <div className="flex items-center gap-2">
+                <IconUpVote className="opacity-60 h-4" />
+                <span className="text-[#72C255]">1.2k</span>
+                <IconDownVote className="opacity-60 h-4" />
+                <span className="text-[#F42F53]">372</span>
+                <IconShare className="opacity-60 ml-2" />
+                <IconChatMessage className="opacity-60 ml-2" />
+              </div>
+            </div>
+          </div>
+          <div className="h-24 w-24 absolute border-[4px] border-[#388DE2] right-0 top-0 translate-x-[25%] rounded-full translate-y-[-25%] dark:bg-[#131316] bg-white">
+            <Image
+              className="h-full w-full transition-opacity duration-300 rounded-full select-none ring-1 ring-zinc-100/10 hover:opacity-80"
+              src={chatbot?.avatar || ''}
+              alt={chatbot?.avatar || 'ChatAvatar'}
+              height={96}
+              width={96}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="max-w-2xl px-4 mx-auto mt-2">
+        <Accordion type="multiple">
+          {pairs.map((pair: MessagePair, key) => (
+            <AccordionItem key={key} value={pair.userMessage.messageId}>
+              <AccordionTrigger className="hover:no-underline">
+                {user?.profilePicture ? (
+                  <div
+                    className={cn(
+                      'flex h-10 w-10 shrink-0 select-none items-center justify-center rounded-full border shadow'
+                    )}
+                  >
+                    <Image
+                      className="h-8 w-8 transition-opacity duration-300 rounded-full select-none hover:opacity-80"
+                      src={user?.profilePicture || ''}
+                      alt={user?.username ?? 'Avatar'}
+                      height={48}
+                      width={48}
+                    />
+                  </div>
+                ) : (
+                  <div
+                    className={cn(
+                      'flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-full border shadow',
+                      'bg-background'
+                    )}
+                  >
+                    <IconUser />
+                  </div>
+                )}
+                <div className="flex-1 px-1 ml-4 space-y-2 text-left text-[1.25rem] relative">
+                  {pair.userMessage.content}
+                  {/* <span className="opacity-30 ml-4 font-normal">
                   by {user?.username.replace('_', ' ')}
                 </span> */}
-              </div>
-              <ChatMessageActions message={convertMessage(pair.userMessage)} />
-            </AccordionTrigger>
-            <AccordionContent className="relative">
-              <div className="md:ml-16">
-                {pair.chatGptMessage ? (
-                  <ChatMessage message={convertMessage(pair.chatGptMessage)} />
-                ) : (
-                  ''
-                )}
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="relative mt-4">
+                <div className="md:ml-12">
+                  {pair.chatGptMessage ? (
+                    <BrowseChatMessage
+                      message={convertMessage(pair.chatGptMessage)}
+                    />
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </div>
   )
 }
