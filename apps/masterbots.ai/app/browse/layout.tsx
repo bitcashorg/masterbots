8f67eeb8bcbcbd7eb8e4bc9c26be33b73b49b879
Roberto Lucas <andler.dev@gmail.com>
2024-03-07 17:21:05 -0600

    [masterbots.ai] feat(chore): robohash credits

--- apps/masterbots.ai/app/browse/layout.tsx
+++ apps/masterbots.ai/app/browse/layout.tsx
@@ -9,8 +9,16 @@ export default async function BrowseLayout({ children }: BrowseLayoutProps) {
     <BrowseProvider>
       { /* TODO: https://github.com/TheSGJ/nextjs-toploader/issues/66 */}
       {/* <NextTopLoader color="#1ED761" initialPosition={0.20} /> */}
-      <main className="flex flex-col h-[calc(100vh-theme(spacing.16))] ">
-        <section className="overflow-auto group scrollbar w-full">{children}</section>
+      <main className="flex flex-col h-[calc(100vh-theme(spacing.16))]">
+        <section className="overflow-auto group scrollbar w-full">
+          {children}
+          <footer className="flex justify-center items-center opacity-50 h-[64px] whitespace-break-spaces text-xs font-semibold font-['Geist_Mono']">
+            Robot avatars delivered by{' '}
+            <a href="https://robohash.org" target="_blank" rel="noreferrer" className="text-link hover:underline focus-within:underline">
+              robohash.org
+            </a>
+          </footer>
+        </section>
       </main>
     </BrowseProvider>
   )
