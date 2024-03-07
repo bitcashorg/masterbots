0e51cdcea3cf5436aa8c4c83f7128267e130ee2f
Roberto Lucas <andler.dev@gmail.com>
2024-03-07 17:21:05 -0600

    [masterbots.ai] feat(chore): robohash credits

--- apps/masterbots.ai/components/sidebar.tsx
+++ apps/masterbots.ai/components/sidebar.tsx
@@ -2,12 +2,12 @@
 
 import * as React from 'react'
 
+import useClickOutside from '@/lib/hooks/use-click-outside'
 import { useSidebar } from '@/lib/hooks/use-sidebar'
 import { cn } from '@/lib/utils'
-import useClickOutside from '@/lib/hooks/use-click-outside'
 import { CategoryMainTabs } from './category-main-tabs'
 
-export interface SidebarProps extends React.ComponentProps<'div'> {}
+export interface SidebarProps extends React.ComponentProps<'div'> { }
 
 export function Sidebar({ className, children }: SidebarProps) {
   const { isSidebarOpen, isLoading, toggleSidebar } = useSidebar()
@@ -17,15 +17,21 @@ export function Sidebar({ className, children }: SidebarProps) {
     toggleSidebar(false)
   })
   return (
-    <div
+    <aside
       ref={ref}
       data-state={isSidebarOpen && !isLoading ? 'open' : 'closed'}
-      className={cn(className, 'h-full flex-col dark:bg-zinc-950')}
+      className={cn(className, 'h-full flex-col dark:bg-zinc-950 z-[5000]')}
     >
       <CategoryMainTabs />
-      <div className="overflow-y-auto scrollbar h-[calc(100%-3rem)]">
+      <div className="overflow-y-auto scrollbar h-[calc(100%-113px)]">
         {children}
       </div>
-    </div>
+      <footer className="flex justify-center items-center opacity-50 h-[64px] whitespace-break-spaces text-xs font-semibold font-['Geist_Mono']">
+        Robot avatars delivered by{' '}
+        <a href="https://robohash.org" target="_blank" rel="noreferrer" className="text-link hover:underline focus-within:underline">
+          robohash.org
+        </a>
+      </footer>
+    </aside>
   )
 }
