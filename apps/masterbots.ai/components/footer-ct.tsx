b0f78fbab3ed71186e47047a0c70fd72fe5460e3
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-03-26 13:14:05 -0700

    fix: update the footer-ct text font & size

--- apps/masterbots.ai/components/footer-ct.tsx
+++ apps/masterbots.ai/components/footer-ct.tsx
@@ -1,7 +1,7 @@
 export default  function FooterCT() {
     return (
         <footer className="flex justify-center items-center opacity-50 h-[64px] ">
-              <span className="font-['Geist'] text-centerb  items-center font-normal md:text-[20px]  text-md  ">Robot avatars delivered by{' '}
+              <span className="font-['Geist_Mono'] text-centerb  items-center font-normal md:text-[12px]  text-md  ">Robot avatars delivered by{' '}
               <a href="https://robohash.org" target="_blank" rel="noreferrer" className="text-primary mr-2  underline focus-within:underline">
                 robohash.org
               </a>
