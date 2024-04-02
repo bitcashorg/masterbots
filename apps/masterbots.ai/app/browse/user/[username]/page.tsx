7e81f4fc3dee12c7c770c4294faf224fe76b91f0
TopETH <supernathanliu@gmail.com>
2024-04-02 11:41:16 -0400

    fix: user threads routing

---
+++ apps/masterbots.ai/app/browse/user/[username]/page.tsx
@@ -0,0 +1,28 @@
+import { getBrowseThreads } from '@/services/hasura'
+import BrowseUserDetails from '@/components/browse-user-details'
+import BrowseSpecificThreadList from '@/components/browse-specific-thread-list'
+
+const PAGE_SIZE = 50
+
+export default async function BotThreadsPage({
+  params
+}: {
+  params: { username: string }
+}) {
+  const threads = await getBrowseThreads({
+    userName: params.username,
+    limit: PAGE_SIZE
+  })
+  return (
+    <div className="w-full py-5">
+      <BrowseUserDetails user={threads?.[0].user} />
+      <BrowseSpecificThreadList
+        initialThreads={threads}
+        PAGE_SIZE={PAGE_SIZE}
+        query={{
+          userName: params.username
+        }}
+      />
+    </div>
+  )
+}
