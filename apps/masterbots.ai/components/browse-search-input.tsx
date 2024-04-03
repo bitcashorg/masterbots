e6884ecf7b93f566e414c187a07f1909c785088a
Brandon Fernández <31634868+Bran18@users.noreply.github.com>
2024-04-02 20:44:22 -0600

    [masterbots.ai] feat: disclaimer texts (#51)
    * impr(webapp):add footer disclaimer
    
    * impr(webapp):add search box disclaimer
    
    * impr(webapp):add dark color footer disclaimer
    
    * impr(webapp):padding fix

--- apps/masterbots.ai/components/browse-search-input.tsx
+++ apps/masterbots.ai/components/browse-search-input.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client'
 
 import { Button } from '@/components/ui/button'
@@ -8,28 +9,33 @@ import { useBrowse } from '@/lib/hooks/use-browse'
 export function BrowseSearchInput() {
   const { keyword, changeKeyword } = useBrowse()
   return (
-    <div className="flex justify-center w-full py-10 dark:bg-[#09090B]  rounded-3xl gap-4">
-      <div className="relative w-full max-w-[600px] flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center w-full pt-10 pb-4 dark:bg-[#09090B] bg-[#F4F4F5] rounded-lg gap-4">
+      <div className="relative w-full max-w-[600px]">
         <Input
           value={keyword}
           onChange={e => {
             changeKeyword(e.target.value)
           }}
           placeholder="Search any chat with any Bot"
-          className="max-w-[600px]"
-
+          className="w-full"
         />
         {keyword && (
           <Button
             type="reset"
             variant="ghost"
-            className="absolute px-3 right-0 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            className="absolute right-0 px-3 -translate-y-1/2 cursor-pointer top-1/2"
             onClick={() => changeKeyword('')}
             aria-label="Clear search"
           >
             <IconClose className="!h-4 !w-4" />
           </Button>
         )}
+      </div>
+      <div className="w-full text-center">
+        <p className="dark:text-[#83E56A] text-[#BE17E8] text-xs font-medium pt-4">
+          Masterbots isn't infallible; verify crucial facts. Responses are for
+          educational use, not legal, medical, financial or specialized advice.
+        </p>
       </div>
     </div>
   )
