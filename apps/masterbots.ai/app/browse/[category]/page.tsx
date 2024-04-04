18ffb175bf68e566a480d943f9ebd5960dc8a35f
Nathanael Liu <supernathanliu@gmail.com>
2024-04-04 03:29:30 +0000

    impr(masterbots.ai): category and bot routing (#58)
    * fix: category and bot routing
    
    * fix:  rm bot avatars or user avatars on browse detail pages
    
    * fix: category status updating issue when refreshing the page
    
    * [coderabitai] impr(masterbots.ai): upt apps/masterbots.ai/components/browse-category-tabs.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Roberto Lucas <andre.rlucas@outlook.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ apps/masterbots.ai/app/browse/[category]/page.tsx
@@ -0,0 +1,27 @@
+import BrowseList from '@/components/browse-list'
+import { BrowseCategoryTabs } from '@/components/browse-category-tabs'
+import { BrowseSearchInput } from '@/components/browse-search-input'
+import { CategoryMainTabs } from '@/components/category-main-tabs'
+import { getCategories } from '@/services/hasura'
+
+export const revalidate = 3600 // revalidate the data at most every hour
+
+export default async function BrowseCategoryPage({
+  params
+}: {
+  params: { category: string }
+}) {
+  const categories = await getCategories()
+
+  return (
+    <div className="max-w-[1024px] pb-10 mx-auto w-full">
+      <CategoryMainTabs />
+      <BrowseCategoryTabs
+        initialCategory={params.category}
+        categories={categories}
+      />
+      <BrowseSearchInput />
+      <BrowseList />
+    </div>
+  )
+}
