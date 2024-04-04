1a318e77f0efbb3020148b5b8d6eade1ce37da4b
Nathanael Liu <supernathanliu@gmail.com>
2024-04-04 03:29:30 +0000

    impr(masterbots.ai): category and bot routing (#58)
    * fix: category and bot routing
    
    * fix:  rm bot avatars or user avatars on browse detail pages
    
    * fix: category status updating issue when refreshing the page
    
    * [coderabitai] impr(masterbots.ai): upt apps/masterbots.ai/components/browse-category-tabs.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Roberto Lucas <andre.rlucas@outlook.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/masterbots.ai/components/browse-list-item.tsx
+++ apps/masterbots.ai/components/browse-list-item.tsx
@@ -16,13 +16,15 @@ export default function BrowseListItem({
   loadMore,
   loading,
   isLast,
-  hasMore
+  hasMore,
+  pageType = ''
 }: {
   thread: Thread
   loadMore: () => void
   loading: boolean
   isLast: boolean
   hasMore: boolean
+  pageType?: string
 }) {
   const threadRef = React.useRef<HTMLDivElement>(null)
   const router = useRouter()
@@ -70,7 +72,7 @@ export default function BrowseListItem({
 
   const goToThread = () => {
     router.push(
-      `/browse/${thread.chatbot.name.trim().toLowerCase()}/${thread.threadId}`
+      `/b/${thread.chatbot.name.trim().toLowerCase()}/${thread.threadId}`
     )
     router.refresh()
   }
@@ -97,9 +99,9 @@ export default function BrowseListItem({
             'relative flex items-center font-normal md:text-lg transition-all w-full gap-3 pr-4'
           )}
         >
-          {thread.chatbot?.avatar ? (
+          {pageType !== 'bot' && thread.chatbot?.avatar ? (
             <Link
-              href={`/browse/${thread.chatbot.name.toLowerCase()}`}
+              href={`/b/${thread.chatbot.name.toLowerCase()}`}
               title={thread.chatbot?.name}
               className={cn(
                 'flex size-8 shrink-0 select-none items-center justify-center rounded-full border shadow'
@@ -114,16 +116,18 @@ export default function BrowseListItem({
               />
             </Link>
           ) : (
-            <Link
-              href={`/browse/${thread.chatbot?.chatbotId}`}
-              title={thread.chatbot?.name}
-              className={cn(
-                'flex size-8 shrink-0 select-none items-center justify-center rounded-full border shadow',
-                'bg-primary text-primary-foreground'
-              )}
-            >
-              <IconOpenAI />
-            </Link>
+            pageType !== 'bot' && (
+              <Link
+                href={`/b/${thread.chatbot?.name.toLowerCase()}`}
+                title={thread.chatbot?.name}
+                className={cn(
+                  'flex size-8 shrink-0 select-none items-center justify-center rounded-full border shadow',
+                  'bg-primary text-primary-foreground'
+                )}
+              >
+                <IconOpenAI />
+              </Link>
+            )
           )}
           <div className="w-[calc(100%-64px)] m:w-[calc(100%-28px)] flex items-center gap-3 text-left">
             <div
@@ -133,10 +137,12 @@ export default function BrowseListItem({
             >
               {thread.messages?.[0]?.content}
             </div>
-            <span className="opacity-50 text-[0.875rem]">by</span>
-            {thread.user?.profilePicture ? (
+            {pageType !== 'user' && (
+              <span className="opacity-50 text-[0.875rem]">by</span>
+            )}
+            {pageType !== 'user' && thread.user?.profilePicture ? (
               <Link
-                href={`/user/${encodeURIComponent(String(thread.user.slug))}`}
+                href={`/u/${encodeURIComponent(String(thread.user.slug))}`}
                 title={thread.user?.username.replace('_', ' ')}
                 className={cn(
                   'flex size-8 shrink-0 select-none items-center justify-center rounded-full border shadow'
@@ -151,16 +157,18 @@ export default function BrowseListItem({
                 />
               </Link>
             ) : (
-              <Link
-                href={`/user/${encodeURIComponent(String(thread.user?.slug))}`}
-                title={thread.user?.username}
-                className={cn(
-                  'flex size-8 shrink-0 select-none items-center justify-center rounded-full border shadow',
-                  'bg-background'
-                )}
-              >
-                <IconUser />
-              </Link>
+              pageType !== 'user' && (
+                <Link
+                  href={`/u/${encodeURIComponent(String(thread.user?.slug))}`}
+                  title={thread.user?.username}
+                  className={cn(
+                    'flex size-8 shrink-0 select-none items-center justify-center rounded-full border shadow',
+                    'bg-background'
+                  )}
+                >
+                  <IconUser />
+                </Link>
+              )
             )}
           </div>
         </div>
