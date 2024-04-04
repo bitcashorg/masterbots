51d18aa44b905457bc0c220f073bf441c338bd35
Roberto Romero Lucas <andre.rlucas@outlook.com>
2024-04-04 11:42:04 -0600

    fix(masterbots.ai): credits on chat scrolling

--- apps/masterbots.ai/app/(chat)/layout.tsx
+++ apps/masterbots.ai/app/(chat)/layout.tsx
@@ -12,10 +12,9 @@ export default async function ChatLayout({ children }: ChatLayoutProps) {
       {/* TODO: https://github.com/TheSGJ/nextjs-toploader/issues/66 */}
       {/* <NextTopLoader color="#1ED761" initialPosition={0.20} /> */}
       <ResponsiveSidebar />
-      <ChatLayoutSection>{children}</ChatLayoutSection>
-      <div className="md:absolute max-w-[240px] text-center top-2 right-4 mx-auto">
-        <FooterCT />
-      </div>
+      <ChatLayoutSection>
+        {children}
+      </ChatLayoutSection>
     </main>
   )
 }
--- apps/masterbots.ai/components/footer-ct.tsx
+++ apps/masterbots.ai/components/footer-ct.tsx
@@ -1,6 +1,16 @@
-export default function FooterCT() {
-  return (
+import { ElementType } from "react"
+
+export default function FooterCT({ nonFooterTag }: { nonFooterTag?: boolean }) {
+  const Footer: ElementType = ({ children }) => nonFooterTag ?
+    <div className="min-h-[64px] whitespace-break-spaces text-xs font-semibold font-['Geist_Mono'] flex justify-center items-center opacity-50">
+      {children}
+    </div> :
     <footer className="min-h-[64px] whitespace-break-spaces text-xs font-semibold font-['Geist_Mono'] flex justify-center items-center opacity-50">
+      {children}
+    </footer>
+
+  return (
+    <Footer>
       <span className="font-['Geist_Mono'] text-centerb  items-center font-normal md:text-[12px]  text-md  ">Robot avatars delivered by{' '}
         <a href="https://robohash.org" target="_blank" rel="noreferrer" className="text-primary mr-2  underline focus-within:underline">
           robohash.org
@@ -10,6 +20,6 @@ export default function FooterCT() {
           terms & policies
         </a>
       </span>
-    </footer>
+    </Footer>
   )
 }
