ac52e5862593ea332ad50361c61e07ad9eb2e9c2
Roberto Romero Lucas <andre.rlucas@outlook.com>
2024-04-04 14:36:49 -0600

    fix(masterbots.ai): thread pop-up device height

--- apps/masterbots.ai/components/chat-layout-section.tsx
+++ apps/masterbots.ai/components/chat-layout-section.tsx
@@ -10,7 +10,7 @@ export function ChatLayoutSection({ children }: { children: React.ReactNode }) {
   return (
     <section
       ref={sectionRef as React.Ref<HTMLDivElement>}
-      className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out lg:ml-[250px] xl:ml-[300px] relative"
+      className="h-[calc(100vh-4rem)] group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out lg:ml-[250px] xl:ml-[300px] relative"
     >
       <div
         className="flex flex-col items-center lg:w-[calc(100%-250px)] xl:w-[calc(100%-300px)]"
