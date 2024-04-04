7908b249a3bb70cee8b6b6b69b5a381294ed3a41
TopETH <supernathanliu@gmail.com>
2024-04-04 11:24:24 -0400

    fix: graphql query

--- apps/masterbots.ai/services/hasura/hasura.service.ts
+++ apps/masterbots.ai/services/hasura/hasura.service.ts
@@ -81,19 +81,19 @@ export async function getChatbots({
         limit: limit ? limit : 20,
         ...(offset
           ? {
-            offset
-          }
+              offset
+            }
           : {}),
         ...(categoryId
           ? {
-            where: {
-              categories: {
-                categoryId: {
-                  _eq: categoryId
+              where: {
+                categories: {
+                  categoryId: {
+                    _eq: categoryId
+                  }
                 }
               }
             }
-          }
           : {})
       }
     }
@@ -142,25 +142,25 @@ export async function getThreads({
         limit: limit ? limit : 20,
         ...(offset
           ? {
-            offset
-          }
+              offset
+            }
           : {}),
         ...(chatbotName || categoryId
           ? {
-            where: {
-              chatbot: {
-                ...(chatbotName
-                  ? {
-                    name: { _eq: chatbotName }
-                  }
-                  : {}),
-                ...(categoryId
-                  ? { categories: { categoryId: { _eq: categoryId } } }
-                  : {})
-              },
-              ...(userId ? { userId: { _eq: userId } } : {})
+              where: {
+                chatbot: {
+                  ...(chatbotName
+                    ? {
+                        name: { _eq: chatbotName }
+                      }
+                    : {}),
+                  ...(categoryId
+                    ? { categories: { categoryId: { _eq: categoryId } } }
+                    : {})
+                },
+                ...(userId ? { userId: { _eq: userId } } : {})
+              }
             }
-          }
           : userId
             ? { where: { userId: { _eq: userId } } }
             : {})
@@ -254,12 +254,14 @@ export async function upsertUser({
     slugCount++
     slug = `${baseSlug}${slugCount > 0 ? slugCount : ''}`
   }
-
   const { insertUserOne } = await client.mutation({
     insertUserOne: {
       __args: {
-        object,
-        slug,
+        object: {
+          username,
+          slug,
+          ...object
+        },
         onConflict: {
           constraint: 'user_email_key',
           updateColumns: ['profilePicture']
@@ -320,16 +322,16 @@ export async function getChatbot({
       },
       ...(threads
         ? {
-          threads: {
-            ...everything,
-            messages: {
+            threads: {
               ...everything,
-              __args: {
-                orderBy: [{ createdAt: 'ASC' }]
+              messages: {
+                ...everything,
+                __args: {
+                  orderBy: [{ createdAt: 'ASC' }]
+                }
               }
             }
           }
-        }
         : {})
     }
   })
@@ -368,21 +370,21 @@ export async function getBrowseThreads({
           orderBy: [{ createdAt: 'ASC' }],
           ...(keyword
             ? {
-              where: {
-                _or: [
-                  {
-                    content: {
-                      _iregex: keyword
+                where: {
+                  _or: [
+                    {
+                      content: {
+                        _iregex: keyword
+                      }
+                    },
+                    {
+                      content: {
+                        _eq: keyword
+                      }
                     }
-                  },
-                  {
-                    content: {
-                      _eq: keyword
-                    }
-                  }
-                ]
+                  ]
+                }
               }
-            }
             : ''),
           limit: 2
         }
@@ -398,35 +400,35 @@ export async function getBrowseThreads({
         where: {
           ...(categoryId
             ? {
-              chatbot: {
-                categories: {
-                  categoryId: { _eq: categoryId }
+                chatbot: {
+                  categories: {
+                    categoryId: { _eq: categoryId }
+                  }
                 }
               }
-            }
             : {}),
           ...(chatbotName
             ? {
-              chatbot: {
-                name: { _eq: chatbotName }
+                chatbot: {
+                  name: { _eq: chatbotName }
+                }
               }
-            }
             : {}),
           ...(userId
             ? {
-              userId: {
-                _eq: userId
+                userId: {
+                  _eq: userId
+                }
               }
-            }
             : {}),
           ...(slug
             ? {
-              user: {
-                slug: {
-                  _eq: slug
+                user: {
+                  slug: {
+                    _eq: slug
+                  }
                 }
               }
-            }
             : {}),
           isPublic: { _eq: true }
         },
@@ -455,13 +457,13 @@ export async function getMessages({
         orderBy: [{ createdAt: 'ASC' }],
         ...(limit
           ? {
-            limit
-          }
+              limit
+            }
           : {}),
         ...(offset
           ? {
-            offset
-          }
+              offset
+            }
           : {})
       }
     }
@@ -482,14 +484,14 @@ export async function getChatbotsCount({
       __args: {
         ...(categoryId
           ? {
-            where: {
-              categories: {
-                categoryId: {
-                  _eq: categoryId
+              where: {
+                categories: {
+                  categoryId: {
+                    _eq: categoryId
+                  }
                 }
               }
             }
-          }
           : {})
       }
     }
