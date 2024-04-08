e15ef3edb50630319b83374c56d896ca02812c32
Gabo Esquivel <contact@gaboesquivel.com>
2024-04-08 10:47:41 -0600

    chore: disable getUserProfile

--- apps/masterbots.ai/services/supabase/supa-server.service.ts
+++ apps/masterbots.ai/services/supabase/supa-server.service.ts
@@ -4,26 +4,27 @@ import { createSupabaseServerClient } from './supa-server-client'
 
 export async function getUserProfile(): Promise<UserProfile | null> {
   try {
-    const supabase = await createSupabaseServerClient()
-    const {
-      data: { user }
-    } = await supabase.auth.getUser()
-    if (!user || !user.email) throw new Error('user not found')
+    return null
+    // const supabase = await createSupabaseServerClient()
+    // const {
+    //   data: { user }
+    // } = await supabase.auth.getUser()
+    // if (!user || !user.email) throw new Error('user not found')
 
-    // TODO: use supabase
-    const userProfile = await getUser({
-      email: user.email,
-      adminSecret: process.env.HASURA_GRAPHQL_ADMIN_SECRET || ''
-    })
+    // // TODO: use supabase
+    // const userProfile = await getUser({
+    //   email: user.email,
+    //   adminSecret: process.env.HASURA_GRAPHQL_ADMIN_SECRET || ''
+    // })
 
-    if (!userProfile) throw new Error('user not found')
-    return {
-      userId: userProfile.userId,
-      username: userProfile.username,
-      name: '',
-      email: userProfile.email,
-      image: userProfile.profilePicture || ''
-    }
+    // if (!userProfile) throw new Error('user not found')
+    // return {
+    //   userId: userProfile.userId,
+    //   username: userProfile.username,
+    //   name: '',
+    //   email: userProfile.email,
+    //   image: userProfile.profilePicture || ''
+    // }
   } catch (error) {
     console.log('GET USER PROFILE ERROR', error)
     return null
