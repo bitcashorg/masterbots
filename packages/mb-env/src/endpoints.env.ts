bb054edc0b71b5884d416a881dfa9f97f1438f7a
Gabo Esquivel <contact@gaboesquivel.com>
2024-04-08 22:30:23 -0600

    fix: prod env value

--- packages/mb-env/src/endpoints.env.ts
+++ packages/mb-env/src/endpoints.env.ts
@@ -1,5 +1,5 @@
 export const endpoints = {
-  prod: '',
+  prod: 'https://api.masterbots.ai/v1/graphql',
   test: 'https://dev-api.masterbots.ai/v1/graphql',
   local: 'http://localhost:8080/v1/graphql',
 }
