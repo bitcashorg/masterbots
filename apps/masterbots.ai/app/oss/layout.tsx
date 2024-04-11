1bd8b072f98da2c486c1d6bdde68d8888e9fa892
Gabo Esquivel <contact@gaboesquivel.com>
2024-04-11 00:19:47 -0600

    chore: oss placeholder

---
+++ apps/masterbots.ai/app/oss/layout.tsx
@@ -0,0 +1,49 @@
+import AccountDetails from '@/components/shared/account-details'
+import { UserProfile } from '@/hooks/use-global-store'
+import { getBrowseThreads, getUserInfoFromBrowse } from '@/services/hasura'
+import { createSupabaseServerClient } from '@/services/supabase'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+
+export default async function OssLayout({ children }: OssLayoutProps) {
+  return (
+    <main className="flex flex-col min-h-screen container">
+      <div className="py-12 px-6">
+        <div className="space-y-0.5 mb-10 pb-10 border-b-[1px] dark:border-mirage border-gray-300">
+          <h1 className="text-2xl font-bold tracking-tight">
+            Open Source Manifest
+          </h1>
+          <p className="text-muted-foreground">Why and how to collab</p>
+        </div>
+        <div className="flex gap-8">
+          <div className="flex min-w-[150px]">
+            <nav aria-label="Sidebar" className="space-y-1">
+              <div className="space-y-3">
+                <Link
+                  className="block text-base font-medium text-gray-300 hover:text-white"
+                  href="/Oss"
+                >
+                  Some data
+                </Link>
+                <Link
+                  className="block text-base font-medium text-gray-300 hover:text-white"
+                  href="/Oss/appereance"
+                >
+                  Some link
+                </Link>
+                {/* Add more links as needed */}
+              </div>
+            </nav>
+          </div>
+          <div className="flew grow space-y-6">{children}</div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+interface OssLayoutProps {
+  children: React.ReactNode
+  params: { category: string }
+}
---
+++ apps/masterbots.ai/app/oss/page.tsx
@@ -0,0 +1,28 @@
+export default async function TermPoliciesPage() {
+  return (
+    <div className="max-w-[1024px] pb-10 mx-auto w-full  mt-10">
+      <div className="max-w-2xl mx-auto">
+        <h1 className="text-2xl font-bold mb-6">Lets go!</h1>
+
+        <h2 className="text-xl font-bold mb-2">Introduction:</h2>
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel
+          purus sed lacus sodales dapibus. Vestibulum hendrerit dui lacus, ut
+          laoreet nulla malesuada a. Aenean quis pharetra justo. Praesent luctus
+          elementum dolor id ultrices. Duis non dapibus magna, tincidunt
+          vestibulum orci. Pellentesque congue tellus suscipit tellus congue, eu
+          vestibulum sem varius. Fusce nec eros sem. Vestibulum ante ipsum
+          primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">Contact Us</h2>
+        <p className="mb-4">
+          For any questions or concerns regarding these Terms, please contact us
+          at contact@masterbots.ai
+        </p>
+
+        {/* <!-- Additional sections and content can be added similarly --> */}
+      </div>
+    </div>
+  )
+}
