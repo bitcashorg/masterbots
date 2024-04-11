619bd5945c097438947b26f922b4200929b5718e
Gabo Esquivel <contact@gaboesquivel.com>
2024-04-10 22:43:36 -0600

    debug: vercel build

--- apps/masterbots.ai/app/(browse)/page.tsx
+++ apps/masterbots.ai/app/(browse)/page.tsx
@@ -4,15 +4,16 @@ import { BrowseInput } from '@/components/shared/browse-input'
 import { getBrowseThreads, getCategories } from '@/services/hasura'
 
 export default async function BrowsePage() {
-  const categories = await getCategories()
-  const threads = await getBrowseThreads({
-    limit: 20
-  })
+  // const categories = await getCategories()
+  // const threads = await getBrowseThreads({
+  //   limit: 20
+  // })
   return (
     <div className="container">
-      <CategoryTabs categories={categories} />
+      HELLO
+      {/* <CategoryTabs categories={categories} />
       <BrowseInput />
-      <ThreadList initialThreads={threads} filter={{}} />
+      <ThreadList initialThreads={threads} filter={{}} /> */}
     </div>
   )
 }
