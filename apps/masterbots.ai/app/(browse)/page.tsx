0843287621a1c7355840e6926a87c40dc1ad4195
Gabo Esquivel <contact@gaboesquivel.com>
2024-04-10 23:01:32 -0600

    chore: restore thread list

--- apps/masterbots.ai/app/(browse)/page.tsx
+++ apps/masterbots.ai/app/(browse)/page.tsx
@@ -5,14 +5,14 @@ import { getBrowseThreads, getCategories } from '@/services/hasura'
 
 export default async function BrowsePage() {
   const categories = await getCategories()
-  // const threads = await getBrowseThreads({
-  //   limit: 20
-  // })
+  const threads = await getBrowseThreads({
+    limit: 20
+  })
   return (
     <div className="container">
       <CategoryTabs categories={categories} />
       <BrowseInput />
-      {/* <ThreadList initialThreads={threads} filter={{}} /> */}
+      <ThreadList initialThreads={threads} filter={{}} />
     </div>
   )
 }
