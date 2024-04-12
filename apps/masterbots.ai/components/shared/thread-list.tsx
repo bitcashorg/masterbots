45c4ca784474971b4824ade7e7af8438df773831
Gabo Esquivel <contact@gaboesquivel.com>
2024-04-11 19:09:09 -0600

    fix: infinite scroll

--- apps/masterbots.ai/components/shared/thread-list.tsx
+++ apps/masterbots.ai/components/shared/thread-list.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import type { Thread } from '@repo/mb-genql'
 import { getBrowseThreads } from '@/services/hasura'
 import { ThreadDoubleAccordion } from './thread-double-accordion'
@@ -8,6 +8,8 @@ import { ThreadDialog } from './thread-dialog'
 import { usePathname, useSearchParams } from 'next/navigation'
 import { GetBrowseThreadsParams } from '@/services/hasura/hasura.service.type'
 import { useQuery } from '@tanstack/react-query'
+import { uniq } from 'lodash'
+import { useAsyncFn } from 'react-use'
 
 export function ThreadList({
   initialThreads,
@@ -16,35 +18,41 @@ export function ThreadList({
   currentThread,
   dialog = false
 }: ThreadListProps) {
-  const searchParams = useSearchParams()
-  const queryKey = [usePathname() + searchParams.get('query')]
+  const params = useSearchParams()
+  const [threads, setThreads] = useState(initialThreads)
+
   const loadMoreRef = useRef<HTMLDivElement>(null)
-  const threads = useQuery({
-    queryKey,
-    queryFn: async () => {
-      const searchFilter = {
-        ...filter,
-        offset: threads.length,
-        limit: 25
-      }
-      console.log(`🛜 Loading More Content for ${JSON.stringify(searchFilter)}`)
-      const moreThreads = await getBrowseThreads(searchFilter)
-      // concatenate newly loaded threads
-      return threads.concat(moreThreads)
-    },
-    // data comes from nextjs server on first render
-    refetchOnMount: false,
-    initialData: initialThreads
-  })
+  const [moreThreads, fetchMoreThreads] = useAsyncFn(() => {
+    const searchFilter = {
+      ...filter,
+      offset: threads.length,
+      limit: 25
+    }
+    console.log(
+      `🛜 Loading More Content for ${JSON.stringify(searchFilter)}, current thread count ${threads.length}`
+    )
+    return getBrowseThreads(searchFilter)
+  }, [filter, threads])
+
+  // add more threads as they come in
+  useEffect(() => {
+    setThreads(prev =>
+      uniq(prev.concat(moreThreads.value)).filter(v => v != undefined)
+    )
+  }, [moreThreads])
 
   // load mare item when it gets to the end
   useEffect(() => {
     if (!loadMoreRef.current) return
     const observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting && !threads.isLoading) {
+      if (entry.isIntersecting && !moreThreads.loading) {
+        console.log(' intersection !')
         // NOTE: I think this is not required
-        // setTimeout(() => threads.refetch(), 150)
-        threads.refetch()
+
+        setTimeout(() => {
+          console.log('go fetch more')
+          fetchMoreThreads()
+        }, 150)
         observer.unobserve(entry.target)
       }
     })
@@ -55,19 +63,25 @@ export function ThreadList({
       // always unsubscribe on component unmount
       observer.disconnect()
     }
-  }, [threads.isLoading])
+  }, [moreThreads.loading])
 
   // ThreadDialog and ThreadDoubleAccordion can be used interchangeably
   const ThreadComponent = dialog ? ThreadDialog : ThreadDoubleAccordion
+
+  // useQuery format. we want to use useInfiniteScroll
+  const queryKey = [
+    usePathname() + params.get('query'),
+    { ...filter, offset: threads.length }
+  ]
 
   return (
     <div
       // use url queryKey as key for component to force rerender
       // I'm debuggin ssr results hydration issues
-      key={queryKey[0]}
+      key={JSON.stringify(queryKey)}
       className="flex flex-col w-full gap-8 py-5"
     >
-      {threads.data.map((thread: Thread) => (
+      {threads.map((thread: Thread) => (
         <ThreadComponent
           key={thread.threadId}
           thread={thread}
