fc0f60836ccf470994e27cfb5d3b33aad07316b7
TopETH <supernathanliu@gmail.com>
2024-04-12 17:37:56 -0400

    feat: update url when opening the thread on browse view

---
+++ apps/masterbots.ai/app/browse/[category]/[threadId]/page.tsx
@@ -0,0 +1,11 @@
+import { getThread } from '@/services/hasura'
+import { BrowseThread } from '@/components/browse-thread'
+import { ChatPageProps } from '@/app/(chat)/[chatbot]/[threadId]/page'
+
+export default async function ChatPage({ params }: ChatPageProps) {
+  const thread = await getThread({
+    threadId: params.threadId,
+    jwt: ''
+  })
+  return <BrowseThread thread={thread} />
+}
