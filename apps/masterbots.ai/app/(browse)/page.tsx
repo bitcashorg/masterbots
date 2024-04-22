6b52ef70c7544639e58a14c0bc4944e5879bb114
Gabo Esquivel <contact@gaboesquivel.com>
2024-04-21 20:12:07 -0600

    wip: supabase text search

--- apps/masterbots.ai/app/(browse)/page.tsx
+++ apps/masterbots.ai/app/(browse)/page.tsx
@@ -6,6 +6,7 @@ import { Card } from '@/components/ui/card'
 import { decodeQuery } from '@/lib/url'
 import { permanentRedirect } from 'next/navigation'
 import { getThreadLink } from '@/lib/threads'
+import { getThreads } from '../actions'
 
 export default async function HomePage({ searchParams }: HomePageProps) {
   if (searchParams.threadId) {
@@ -13,33 +14,35 @@ export default async function HomePage({ searchParams }: HomePageProps) {
     permanentRedirect(getThreadLink({ thread }))
   }
 
+  // TODO: move this static generation phase
   const categories = await getCategories()
+
   const query = searchParams.query ? decodeQuery(searchParams.query) : null
   const limit = searchParams.limit ? parseInt(searchParams.limit) : 20
   const page = searchParams.page ? parseInt(searchParams.page) : 1
+  console.log('hey hey hey', { query, limit, page })
+  const formData = new FormData()
 
-  const threads = await getBrowseThreads({
-    limit,
-    offset: (page - 1) * limit,
-    query
-  })
+  // Append the limit parameter to the FormData
+  formData.append('query', query)
+  const threads = await getThreads(formData)
 
   return (
     <div className="container">
       <CategoryTabs categories={categories} />
       <SearchInput />
-      {/* <div>Your query: {query}</div>
+      <div>Your query: {query}</div>
       <ul>
         {threads.map(t => (
           <li key={t.threadId}>{t.messages[0]?.content || 'not found'}</li>
         ))}
-      </ul> */}
+      </ul>
 
-      {threads.length ? (
+      {/* {threads.length ? (
         <ThreadList initialThreads={threads} />
       ) : (
         <Card>no results</Card>
-      )}
+      )} */}
     </div>
   )
 }
--- apps/masterbots.ai/app/actions.ts
+++ apps/masterbots.ai/app/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { createSupabaseServerClient } from '@/services/supabase'
 import { Dub } from 'dub'
 
 const dub = new Dub({
@@ -32,4 +33,36 @@ export async function shorten(_prevState: any, formData: any) {
       shortLink: 'Invalid data provided'
     }
   }
+}
+
+/*
+ The base query is set up to fetch all threads and all associated messages, ordered by creation time.
+ When a search query is provided, a modified select statement is used which applies a filter to exclude messages 
+ with a role of 'system' only during the search. This way, the search will ignore 'system' messages but the
+ final output for threads that match other criteria will include all messages.
+ Conditional Query Construction: The filter and text search are only applied if there's a non-empty searchQuery.
+ This ensures that all messages, including those with a 'system' role, are returned if there is no search criterion.
+*/
+export async function getThreads(formData: FormData) {
+  const supabase = await createSupabaseServerClient()
+  const query = formData.get('query')
+
+  let supaQuery = supabase.from('thread').select(
+    `
+      *,
+      message (
+        * order(created_at desc)
+      )
+    `
+  )
+
+  if (query) {
+    supaQuery = supaQuery
+      .filter('message.role', 'neq', 'system')
+      .textSearch('message.content', query.toString())
+  }
+
+  const { data, error } = await supaQuery
+
+  return error ? null : data
 }
