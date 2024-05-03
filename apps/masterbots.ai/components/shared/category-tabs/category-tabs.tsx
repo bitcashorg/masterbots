d982bfda3c67552d2fc954c8967c329e757c6f26
Nathanael Liu <supernathanliu@gmail.com>
2024-05-03 03:38:22 +0000

    fix: restore classname for active tab in categories tab

--- apps/masterbots.ai/components/shared/category-tabs/category-tabs.tsx
+++ apps/masterbots.ai/components/shared/category-tabs/category-tabs.tsx
@@ -2,6 +2,8 @@
 
 import type { Category } from '@repo/mb-genql'
 import { CategoryLink } from './category-link'
+import { useEffect } from 'react'
+import { toSlug } from '@/lib/url'
 
 export function CategoryTabs({
   categories,
@@ -10,11 +12,38 @@ export function CategoryTabs({
   categories: Category[]
   initialCategory?: string
 }) {
+  useEffect(() => {
+    console.log('TopETH', initialCategory)
+    if (document) {
+      const element = document.getElementById(
+        `browse-category-tab__${
+          initialCategory === 'all'
+            ? 'all'
+            : categories.filter(c => toSlug(c.name) === initialCategory)[0]
+                ?.categoryId
+        }`
+      )
+
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+          inline: 'center'
+        })
+      }
+    }
+  }, [initialCategory])
+
   return (
     <div className="w-full py-[10px] my-3 !overflow-x-auto overflow-y-hidden whitespace-nowrap scrollbar small-thumb justify-between flex">
-      <CategoryLink category="all" id="browse-category-tab__null" />
+      <CategoryLink
+        activeTab={initialCategory}
+        category="all"
+        id="browse-category-tab__all"
+      />
       {categories.map((category, key) => (
         <CategoryLink
+          activeTab={initialCategory}
           category={category}
           id={`browse-category-tab__${category.categoryId}`}
           key={key}
