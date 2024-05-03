0d622f6de228a8180bffa04dd89094e56858133c
Nathanael Liu <supernathanliu@gmail.com>
2024-05-03 13:13:09 -0700

    fix(masterbots.ai): restore classname for active tab in categories tab (#208)

--- apps/masterbots.ai/components/shared/category-tabs/category-link.tsx
+++ apps/masterbots.ai/components/shared/category-tabs/category-link.tsx
@@ -5,15 +5,18 @@ import { toSlug } from '@/lib/url'
 
 export function CategoryLink({
   category,
-  id
+  id,
+  activeTab
 }: {
   category: Category | 'all'
   id: string
+  activeTab: string
 }) {
   return (
     <Link
       className={`${
-        category === 'all'
+        (activeTab === 'all' && category === 'all') ||
+        (category !== 'all' && activeTab === toSlug(category.name))
           ? 'dark:text-white'
           : 'dark:hover:text-white dark:text-[#F4F4F580] text-zinc-500 hover:text-black'
       } relative rounded-full px-3 py-1.5 text-sm font-medium  outline-sky-400 transition focus-visible:outline-2`}
@@ -24,14 +27,14 @@ export function CategoryLink({
         WebkitTapHighlightColor: 'transparent'
       }}
     >
-      {/* {((activeTab === null && category === 'all') ||
-        (category !== 'all' && activeTab === category.categoryId)) && (
+      {((activeTab === 'all' && category === 'all') ||
+        (category !== 'all' && activeTab === toSlug(category.name))) && (
         <motion.span
-          className="absolute inset-0 z-10 bg-transparent border-b-2 border-[#388DE2]"
+          className="absolute inset-0 z-10 bg-transparent border-solid border-b-2 border-[#388DE2]"
           layoutId="bubble"
           transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
         />
-      )} */}
+      )}
       {category === 'all' ? 'All' : category.name}
     </Link>
   )
