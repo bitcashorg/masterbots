a64fec76cfb4898f2d18729b22dd52cb6e9ece8d
Roberto Lucas <andre.rlucas@outlook.com>
2024-05-07 09:17:19 -0600

    chore: webapp ux impr (#211)
    * impr(masterbots.ai): add return to browse on bot thread page view (#204)
    
    * ✨ Added back button to thread details page
    
    * ⚡️  changed char to svg
    
    * feat: ai gen 404 image for custom 404 error page  (#210)
    
    * ⚡️ added custom  error page
    
    * ⚡️  clean up
    
    ---------
    
    Co-authored-by: Jimoh sherifdeen <63134009+sheriffjimoh@users.noreply.github.com>

---
+++ apps/masterbots.ai/app/error.tsx
@@ -0,0 +1,45 @@
+'use client' 
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Error({
+  error
+}: {
+  error: Error & { digest?: string }
+}) {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="w-80 h-80 md:w-96 md:h-96 relative">
+        <Image
+          src="/images/404.png"
+          alt="404 Not Found"
+          layout="fill"
+          objectFit="contain"
+          className="w-full h-full"
+        />
+      </div>
+      <h1 className="text-3xl font-bold  mb-4">Oops! Page Not Found</h1>
+      <p className=" text-center mb-4">{error.message}</p>
+      <Link href="/" className='flex items-center space-x-1'>
+        <svg
+          width="11"
+          height="12"
+          viewBox="0 0 11 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.09973 2.15008L3.24979 6.00003L7.09973 9.84998"
+            stroke="#FAFAFA"
+            stroke-width="0.962486"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>{' '}
+        <span >Return Home</span>
+        
+      </Link>
+    </div>
+  )
+}
