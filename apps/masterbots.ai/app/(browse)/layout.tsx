40297fea8143e3ec66dbef9650c6be26fc3beb77
Roberto Lucas <andler.dev@gmail.com>
2024-05-07 17:42:32 -0600

    fix(masterbots.ai): terms page visibility and access

--- apps/masterbots.ai/app/(browse)/layout.tsx
+++ apps/masterbots.ai/app/(browse)/layout.tsx
@@ -1,5 +1,5 @@
-import { BrowseProvider } from '@/lib/hooks/use-browse';
 import FooterCT from '@/components/footer-ct';
+import { BrowseProvider } from '@/lib/hooks/use-browse';
 
 interface BrowseLayoutProps {
   children: React.ReactNode
@@ -10,10 +10,10 @@ export default async function BrowseLayout({ children }: BrowseLayoutProps) {
     <BrowseProvider>
       { /* TODO: https://github.com/TheSGJ/nextjs-toploader/issues/66 */}
       {/* <NextTopLoader color="#1ED761" initialPosition={0.20} /> */}
-      <main className="flex flex-col h-[calc(100vh-theme(spacing.16))]">
+      <main className="flex flex-col w-full h-[calc(100vh-theme(spacing.16))]">
         <section className="overflow-auto group scrollbar w-full">
           {children}
-         <FooterCT />
+          <FooterCT fixed />
         </section>
       </main>
     </BrowseProvider>
