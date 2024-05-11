2f75abef39cbfb78dffdd04dbd4893a7083e4229
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-05-11 09:17:26 +0100

    feat: added custom metadata to bot page

--- apps/masterbots.ai/app/og/route.tsx
+++ apps/masterbots.ai/app/og/route.tsx
@@ -20,16 +20,14 @@ export async function GET(req: NextRequest) {
     const username = thread.user?.username
     const user_avatar = thread.user?.profilePicture || ''
 
+    let theme = 'light'
+    if (typeof window !== 'undefined') {
+      theme = localStorage.getItem('theme') || 'dark'
 
-     let theme = 'light'
-     if (typeof window !== 'undefined') {
-        theme = localStorage.getItem('theme') || 'dark'
+      console.log('theme', theme)
+    }
 
-        console.log('theme', theme)
-     }
-
-
-     const isLightTheme = theme === 'light';
+    const isLightTheme = theme === 'light'
 
     return new ImageResponse(
       (
@@ -40,12 +38,11 @@ export async function GET(req: NextRequest) {
             display: 'flex',
             alignItems: 'flex-start',
             justifyContent: 'flex-start',
-            backgroundColor: isLightTheme ? '#ffff': '#17171b',
+            backgroundColor: isLightTheme ? '#ffff' : '#17171b',
             padding: '40px',
-            color: isLightTheme ?  '#17171b' : '#ffff'
+            color: isLightTheme ? '#17171b' : '#ffff'
           }}
         >
-        
           <div
             style={{
               position: 'absolute',
@@ -58,16 +55,19 @@ export async function GET(req: NextRequest) {
           >
             <svg width="100%" height="100%" viewBox="0 0 1200 630" fill="none">
               <g clip-path="url(#clip0_1009_12)">
-                <path d="M1111 -184H111V816H1111V-184Z" fill={isLightTheme ? '#FFFFFF' : '#18181B'}  />
+                <path
+                  d="M1111 -184H111V816H1111V-184Z"
+                  fill={isLightTheme ? '#FFFFFF' : '#18181B'}
+                />
                 <path
                   d="M311 56H-64V431H311V56Z"
-                  stroke={isLightTheme ? '#FFFFFF' : '#18181B'} 
+                  stroke={isLightTheme ? '#FFFFFF' : '#18181B'}
                   stroke-opacity="0.5"
                   stroke-width="12.5"
                 />
                 <path
                   d="M286 81H-39V406H286V81Z"
-                  stroke={isLightTheme ? '#FFFFFF' : '#18181B'} 
+                  stroke={isLightTheme ? '#FFFFFF' : '#18181B'}
                   stroke-opacity="0.5"
                   stroke-width="12.5"
                 />
@@ -422,10 +422,10 @@ export async function GET(req: NextRequest) {
                   y2="3.6827e-05"
                   gradientUnits="userSpaceOnUse"
                 >
-                  <stop stop-color={isLightTheme ? '#FFFFFF' : '#18181B'}  />
+                  <stop stop-color={isLightTheme ? '#FFFFFF' : '#18181B'} />
                   <stop
                     offset="0.513001"
-                    stop-color={isLightTheme ? '#FFFFFF' : '#18181B'} 
+                    stop-color={isLightTheme ? '#FFFFFF' : '#18181B'}
                     stop-opacity="0"
                   />
                 </linearGradient>
@@ -472,7 +472,7 @@ export async function GET(req: NextRequest) {
                 style={{
                   fontSize: '64px',
                   lineHeight: '75px',
-                     color: isLightTheme ? '#17171b' : '#ffff',
+                  color: isLightTheme ? '#17171b' : '#ffff',
                   overflow: 'hidden',
                   textOverflow: 'ellipsis',
                   display: '-webkit-box',
@@ -486,7 +486,7 @@ export async function GET(req: NextRequest) {
               <p
                 style={{
                   fontSize: '24px',
-                     color: isLightTheme ? '#17171b' : '#ffff',
+                  color: isLightTheme ? '#17171b' : '#ffff',
                   overflow: 'hidden',
                   textOverflow: 'ellipsis',
                   display: '-webkit-box',
@@ -511,13 +511,14 @@ export async function GET(req: NextRequest) {
                     objectFit: 'cover',
                     width: '50px',
                     height: '50px',
-                    borderRadius: '50%'
+                    borderRadius: '50%',
+                    border: '2px solid #388DE2'
                   }}
                   src={user_avatar}
                 />
                 <p
                   style={{
-                       color: isLightTheme ? '#17171b' : '#ffff',
+                    color: isLightTheme ? '#17171b' : '#ffff',
                     fontSize: '34px',
                     marginLeft: '15px',
                     marginTop: '40px',
@@ -530,372 +531,6 @@ export async function GET(req: NextRequest) {
             </div>
             {thread.chatbot.avatar ? (
               <div style={{ display: 'flex' }}>
-                {/* <div
-                  style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                    display: 'flex'
-                  }}
-                >
-                  <svg
-                    width="100%"
-                    height="100%"
-                    viewBox="0 0 1200 630"
-                    fill="none"
-                  >
-                    <path
-                      d="M311 56H-64V431H311V56Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M286 81H-39V406H286V81Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M261 106H-14V381H261V106Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M236 131H11V356H236V131Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M211 156H36V331H211V156Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M186 181H61V306H186V181Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M161 206H86V281H161V206Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M136 231H111V256H136V231Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M311 -144H136V31H311V-144Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M286 -119H161V6H286V-119Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M711 -144H336V231H711V-144Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M686 -119H361V206H686V-119Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M661 -94H386V181H661V-94Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M636 -69H411V156H636V-69Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M611 -44H436V131H611V-44Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M586 -19H461V106H586V-19Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M561 6H486V81H561V6Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M536 31H511V56H536V31Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M511 256H336V431H511V256Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M486 281H361V406H486V281Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M461 306H386V381H461V306Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M436 331H411V356H436V331Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1111 -344H736V31H1111V-344Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1086 -319H761V6H1086V-319Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M911 56H736V231H911V56Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M886 81H761V206H886V81Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M861 106H786V181H861V106Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M836 131H811V156H836V131Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M511 456H136V831H511V456Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M486 481H161V806H486V481Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M461 506H186V781H461V506Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M436 531H211V756H436V531Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M411 556H236V731H411V556Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M386 581H261V706H386V581Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M361 606H286V681H361V606Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M336 631H311V656H336V631Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1511 456H1136V831H1511V456Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M911 256H536V631H911V256Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M886 281H561V606H886V281Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M861 306H586V581H861V306Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M836 331H611V556H836V331Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M811 356H636V531H811V356Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M786 381H661V506H786V381Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M761 406H686V481H761V406Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M736 431H711V456H736V431Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1311 56H936V431H1311V56Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1286 81H961V406H1286V81Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1261 106H986V381H1261V106Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1236 131H1011V356H1236V131Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1211 156H1036V331H1211V156Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1186 181H1061V306H1186V181Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1161 206H1086V281H1161V206Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1136 231H1111V256H1136V231Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1111 456H936V631H1111V456Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1086 481H961V606H1086V481Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1061 506H986V581H1061V506Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1036 531H1011V556H1036V531Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                    <path
-                      d="M1311 -144H1136V31H1311V-144Z"
-                      stroke="#71717A"
-                      stroke-opacity="0.5"
-                      stroke-width="12.5"
-                    />
-                  </svg>
-                </div> */}
                 <div
                   style={{
                     backgroundColor: '#1E293B',
@@ -937,6 +572,5 @@ export async function GET(req: NextRequest) {
   }
 }
 function useTheme() {
-    throw new Error('Function not implemented.')
+  throw new Error('Function not implemented.')
 }
-
