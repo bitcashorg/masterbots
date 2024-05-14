11ef557c59d0239d8abe8b7f05cc37331339945c
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-05-14 05:26:31 +0100

    fix: use variable for URL

--- apps/masterbots.ai/lib/metadata.ts
+++ apps/masterbots.ai/lib/metadata.ts
@@ -1,51 +1,50 @@
-import { getThread } from '@/services/hasura';
-import type { Metadata } from 'next';
-import { format } from 'date-fns';
-import { getThreadLink } from './threads';
+import { getThread } from '@/services/hasura'
+import type { Metadata } from 'next'
+import { format } from 'date-fns'
+import { getThreadLink } from './threads'
 
 export async function generateMbMetadata({
-  params,
+  params
 }): Promise<Metadata | undefined> {
-const threadId = params?.threadId
-  const thread = await getThread({threadId, jwt:''})
+  const threadId = params?.threadId
+  const thread = await getThread({ threadId, jwt: '' })
   if (!thread) return
 
-
-   const firstQuestion=
-      thread.messages.find(m => m.role === 'user')?.content || 'not found'
-    const firstResponse =
-      thread.messages.find(m => m.role === 'assistant')?.content || 'not found'
+  const firstQuestion =
+    thread.messages.find(m => m.role === 'user')?.content || 'not found'
+  const firstResponse =
+    thread.messages.find(m => m.role === 'assistant')?.content || 'not found'
 
   const data = {
     title: firstQuestion,
     publishedAt: thread.updatedAt, // format(thread.updatedAt, 'MMMM dd, yyyy'),
     summary: firstResponse,
-    image: `http://localhost:3000/og?threadId=${thread.threadId}`,
-    pathname: getThreadLink({thread:thread, chat:false})
-  } 
+    image: `${process.env.VERCEL_URL}/og?threadId=${thread.threadId}`,
+    pathname: getThreadLink({ thread: thread, chat: false })
+  }
 
   return {
-    title:data.title,
-    description:data.summary,
+    title: data.title,
+    description: data.summary,
     openGraph: {
       locale: 'en_US',
-      title:data.title,
-      description:data.summary,
+      title: data.title,
+      description: data.summary,
       type: 'article',
       publishedTime: data.publishedAt,
-      url: `http://localhost:3000${data.pathname}`,
+      url: `${process.env.VERCEL_URL}${data.pathname}`,
       images: [
         {
-          url: data.image,
-        },
-      ],
+          url: data.image
+        }
+      ]
     },
     twitter: {
       card: 'summary_large_image',
-      title:data.title,
+      title: data.title,
       site: '@masterbotsai',
-      description:data.summary,
-      images: [data.image],
-    },
-  };
+      description: data.summary,
+      images: [data.image]
+    }
+  }
 }
