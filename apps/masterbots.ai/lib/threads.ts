09e9128a099dbdad1f63a561248d844efd6940c1
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-05-15 22:03:28 +0100

    fix: clean and build fix

--- apps/masterbots.ai/lib/threads.ts
+++ apps/masterbots.ai/lib/threads.ts
@@ -1,49 +1,10 @@
 import type * as AI from 'ai'
-import { Message, Thread } from '@repo/mb-genql'
-import { type Message as AIMessage } from 'ai/react'
+import { Message, Thread } from 'mb-genql'
 import { extractBetweenMarkers } from './utils'
 import { toSlug } from './url'
 
-export function createMessagePairs(messages: Message[] | AIMessage[]) {
-  const messagePairs: MessagePair[] = []
 
-  for (let i = 0; i < messages.length; i++) {
-    const message = messages[i]
 
-    if (message.role === 'user') {
-      const userMessage = message
-      const chatGptMessages = []
-      for (let j = i + 1; j < messages.length; j++) {
-        const chatGptMessage = findNextAssistantMessage(messages, j)
-        if (!chatGptMessage) {
-          break
-        } else {
-          chatGptMessages.push(chatGptMessage)
-          continue
-        }
-      }
-      messagePairs.push({
-        userMessage,
-        chatGptMessage: chatGptMessages
-      })
-    }
-  }
-
-  return messagePairs
-}
-
-const findNextAssistantMessage = (
-  messages: Message[] | AIMessage[],
-  startIndex: number
-) => {
-  if (messages[startIndex].role === 'assistant') {
-    return {
-      ...messages[startIndex],
-      content: cleanPrompt(messages[startIndex].content)
-    }
-  }
-  return null
-}
 
 // From chat-message.tsx
 export function cleanPrompt(str: string) {
