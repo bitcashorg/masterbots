954c3fd483b02f2739527ebeea07168406845fa6
Jimoh sherifdeen <63134009+sheriffjimoh@users.noreply.github.com>
2024-05-20 16:06:05 +0100

    feat(masterbots.ai): consistent og image style design and dynamic metadata  (#215)
    * feat: added og api endpoint
    
    * feat: design og image for dark mode
    
    * fix: file formated
    
    * fix: amend  og image to pick current theme color and adapt
    
    * feat: added custom metadata to thread page
    
    * feat: added custom metadata to bot page
    
    * fix: clean up
    
    * fix: move bg to a component
    
    * fix: move og-image design  to a component
    
    * fix: use variable for URL
    
    * fix: to slug func
    
    * ⚡️ Move and clean up UrlToSlug
    
    * fix(masterbots.ai): zod dependecy
    
    * fix: type error
    
    * fix: type error for metadata
    
    * fix: clean and build fix
    
    ---------
    
    Co-authored-by: Roberto Lucas <andler.dev@gmail.com>

--- apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
+++ apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
@@ -1,6 +1,7 @@
 import { getThread } from '@/services/hasura'
 import { BrowseThread } from '@/components/browse-thread'
 import { ChatPageProps } from '@/app/chat/[chatbot]/[threadId]/page'
+export { generateMbMetadata as generateMetadata } from '@/lib/metadata'
 
 export default async function ChatPage({ params }: ChatPageProps) {
   const thread = await getThread({
---
+++ apps/masterbots.ai/lib/metadata.ts
@@ -0,0 +1,51 @@
+import { getThread } from '@/services/hasura'
+import type { Metadata } from 'next'
+import { getThreadLink } from './threads'
+
+export async function generateMbMetadata({
+  params
+}: {
+  params: any;
+}): Promise<Metadata | undefined> {
+  const threadId = params?.threadId
+  const thread = await getThread({ threadId, jwt: '' })
+  if (!thread) return
+
+  const firstQuestion =
+    thread.messages.find(m => m.role === 'user')?.content || 'not found'
+  const firstResponse =
+    thread.messages.find(m => m.role === 'assistant')?.content || 'not found'
+
+  const data = {
+    title: firstQuestion,
+    publishedAt: thread.updatedAt, // format(thread.updatedAt, 'MMMM dd, yyyy'),
+    summary: firstResponse,
+    image: `${process.env.VERCEL_URL}/og?threadId=${thread.threadId}`,
+    pathname: getThreadLink({ thread: thread, chat: false })
+  }
+
+  return {
+    title: data.title,
+    description: data.summary,
+    openGraph: {
+      locale: 'en_US',
+      title: data.title,
+      description: data.summary,
+      type: 'article',
+      publishedTime: data.publishedAt,
+      url: `${process.env.VERCEL_URL}${data.pathname}`,
+      images: [
+        {
+          url: data.image
+        }
+      ]
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: data.title,
+      site: '@masterbotsai',
+      description: data.summary,
+      images: [data.image]
+    }
+  }
+}
