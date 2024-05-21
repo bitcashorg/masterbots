1b1a93d169e66b826bc9e9e4e882aa3a76eb5e4f
Bran18 <andreyfdez18@gmail.com>
2024-05-21 14:45:19 -0600

    (masterbots.ai)feat:wordware model

--- apps/masterbots.ai/app/api/chat/actions/models.ts
+++ apps/masterbots.ai/app/api/chat/actions/models.ts
@@ -1,9 +1,9 @@
-
 export enum AIModels {
-    Default = 'gpt-3.5-turbo',
-    GPT3 = 'gpt-3',
-    GPT4 = 'gpt-4',
-    Claude3 = 'claude-3-haiku-20240307',
-    llama3_8b= 'llama3-8b-8192',
-    llama3_7b= 'llama3-70b-8192'
-  }
+  Default = 'gpt-3.5-turbo',
+  GPT3 = 'gpt-3',
+  GPT4 = 'gpt-4',
+  Claude3 = 'claude-3-haiku-20240307',
+  llama3_8b = 'llama3-8b-8192',
+  llama3_7b = 'llama3-70b-8192',
+  Wordware = 'wordware-ai'
+}
