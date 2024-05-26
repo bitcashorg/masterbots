3b23e5ffae1c5a2a2ff29789ef1fec0c1864624e
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-05-26 18:11:06 +0100

    feat: added animation to the plan card

--- apps/masterbots.ai/components/plans.tsx
+++ apps/masterbots.ai/components/plans.tsx
@@ -8,9 +8,8 @@ export  function Plans() {
                 <span className="font-bold text-[16px]">Subscribe using  <span className="text-[#635BFF]">Stripe</span> </span>
             </div>
             <div className="flex flex-col w-full mt-2 ">
-               <div  className="bg-cover bg-center border-gradient   p-5"
-                    style={{ backgroundImage: 'url(/free_plan_bg.png)' }} >
-                   <div className="flex justify-between items-center  my-auto ">
+               <div  className="border-gradient  "  >
+                   <div className="flex justify-between items-center inner-content my-auto p-5">
                             <div className="flex flex-col space-y-2 ">
                                 <span className="font-bold text-[13px] text-[#83E56A]">PURCHASED</span>
                                  <div className="text-white space-y-1">
@@ -24,6 +23,8 @@ export  function Plans() {
                    </div>
                 </div>
             </div>
+
         </div>
+
     )
     }
