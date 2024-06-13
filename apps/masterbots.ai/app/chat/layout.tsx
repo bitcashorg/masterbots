7612479ff6c467890a3ccecbbd9b5181fa1ec7f9
Bran18 <andreyfdez18@gmail.com>
2024-06-13 08:01:40 -0600

    [masterbots.ai]feat:(multimodels-integration)add NextTopLoader

--- apps/masterbots.ai/app/chat/layout.tsx
+++ apps/masterbots.ai/app/chat/layout.tsx
@@ -1,6 +1,7 @@
 import { ChatLayoutSection } from '@/components/chat-layout-section'
 import { ResponsiveSidebar } from '@/components/sidebar-responsive'
 import FooterCT from '@/components/footer-ct'
+import NextTopLoader from 'nextjs-toploader'
 
 interface ChatLayoutProps {
   children: React.ReactNode
@@ -9,8 +10,7 @@ interface ChatLayoutProps {
 export default async function ChatLayout({ children }: ChatLayoutProps) {
   return (
     <main className="relative flex flex-col h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
-      {/* TODO: https://github.com/TheSGJ/nextjs-toploader/issues/66 */}
-      {/* <NextTopLoader color="#1ED761" initialPosition={0.20} /> */}
+      <NextTopLoader color="#1ED761" initialPosition={0.20} />
       <ResponsiveSidebar />
       <ChatLayoutSection>
         {children}
