6475f9427c1105c1a078024d71a48e75c2a26104
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-06-24 22:06:01 +0100

    fix: update terms

--- apps/masterbots.ai/app/terms/page.tsx
+++ apps/masterbots.ai/app/terms/page.tsx
@@ -4,16 +4,16 @@ export default async function TermPoliciesPage() {
   return (
     <div className="max-w-[1024px] pb-10 mx-auto w-full  mt-10">
       <div className="max-w-2xl mx-auto">
-        <h1 className="text-2xl font-bold mb-6">Masterbots Privacy Policy</h1>
+        <h1 className="text-2xl font-bold mb-6 uppercase">Masterbots Privacy Policy</h1>
 
 
-        <h2 className="text-xl font-bold mb-2">Introduction:</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Introduction:</h2>
         <p className="mb-4">Welcome to Masterbots, the premier destination for engaging with domain-specific AI-driven
             chatbots. By using our website, you agree to the collection, use, and sharing of your personal information
             as described in this privacy policy. Our service involves the aggregation of chatbot interactions, which
             are publicized by default.</p>
 
-        <h2 className="text-xl font-bold mb-2">Information We Collect:</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Information We Collect:</h2>
         <ul className="list-disc pl-6 mb-4">
             <li>Personal Information: We may collect personal information such as your name, email address, and any
                 other information you provide when you register, submit inquiries, or interact with our chatbots.</li>
@@ -23,7 +23,7 @@ export default async function TermPoliciesPage() {
                 data, browsing preferences, and other interactions with the service.</li>
         </ul>
 
-        <h2 className="text-xl font-bold mb-2">How We Use Your Information:</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">How We Use Your Information:</h2>
         <ul className="list-disc pl-6 mb-4">
             <li>To provide, maintain, and improve our services.</li>
             <li>To publicize chat interactions, as per our platform&apos;s unique feature.</li>
@@ -31,7 +31,7 @@ export default async function TermPoliciesPage() {
             <li>For research and analytics to better understand how users interact with our chatbots and services.</li>
         </ul>
 
-        <h2 className="text-xl font-bold mb-2">Sharing and Disclosure:</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Sharing and Disclosure:</h2>
         <ul className="list-disc pl-6 mb-4">
             <li>Publicizing Chats: As an integral feature of our platform, chats are publicized by default. Users should
                 be aware that any information shared during chat sessions will be publicly accessible.</li>
@@ -42,7 +42,7 @@ export default async function TermPoliciesPage() {
                 agreements.</li>
         </ul>
 
-        <h2 className="text-xl font-bold mb-2">Your Data Protection Rights</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Your Data Protection Rights</h2>
         <p className="mb-4">Depending on your location, you may have rights including:</p>
         <ul className="list-disc pl-6 mb-4">
             <li>Accessing, correcting, updating, or requesting deletion of your personal information.</li>
@@ -50,36 +50,58 @@ export default async function TermPoliciesPage() {
                 the portability of your personal information.</li>
         </ul>
 
-        <h2 className="text-xl font-bold mb-2">Data Security</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Data Security</h2>
         <p className="mb-4">We implement appropriate technical and organizational measures to protect the security of your
             personal information against unauthorized or unlawful processing and against accidental loss, destruction,
             or damage.</p>
 
-        <h2 className="text-xl font-bold mb-2">Children&apos;s Privacy</h2>
+         <h2 className="text-2xl font-bold mb-5 uppercase">SUBSCRIPTION PAYMENT TERMS</h2>
+
+        <h2 className="text-md font-bold mb-2 uppercase">Payment Management</h2>
+        <p className="mb-4">The payment process for our services is managed by Stripe, a leading online payment processing platform.
+         We only store subscription identification details necessary to track active subscriptions for future reference.
+          We do not store any payment data on our servers. All payment information is securely managed by Stripe.</p>
+
+          <h2 className="text-md font-bold mb-2 uppercase">Additional Fees</h2>
+        <p className="mb-4">Please be aware that any additional fees incurred due to country-specific regulations are also managed by Stripe. 
+        These fees are determined by Stripe's pricing and fee structure, which includes factors such as currency conversion fees, international card transaction fees, and other applicable charges. For more detailed information about Stripe's pricing and fees,
+         please refer to their official <a className="underline" href="https://stripe.com/resources/more/payment-gateway-fees#:~:text=Stripe's%20payment%20gateway%20fees,-Here's%20a%20list&text=Transaction%20fees%3A%20Stripe%20charges%202.9,if%20currency%20conversion%20is%20necessary.">Pricing and Fees Guide</a> .</p>
+
+        <h2 className="text-md font-bold mb-2 uppercase">DISCLAIMER</h2>
+        <p className="mb-4">We are not responsible for any additional fees imposed by country regulations or Stripe's pricing policies. All transactions are subject to the terms and conditions set forth by Stripe. By using our services,
+         you agree to comply with Stripe's payment terms and policies. </p>
+
+         <h2 className="text-md font-bold mb-2 uppercase">USER RESPONSIBILITIES</h2>
+         <p className="mb-4">As a user, it is your responsibility to ensure that your payment information is accurate and up-to-date.
+          Any disputes regarding additional fees should be directed to Stripe's support team for resolution. 
+          For further details, please refer to our <a className="underline" href="/faq">FAQ page</a>  for general information about product subscriptions and country-specific regulations</p>
+
+
+        <h2 className="text-xl font-bold mb-2 uppercase">Children&apos;s Privacy</h2>
         <p className="mb-4">Our service is not directed to individuals under the age of 13. We do not knowingly collect
             personal information from children under 13.</p>
 
-        <h2 className="text-xl font-bold mb-2">Changes to This Privacy Policy</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Changes to This Privacy Policy</h2>
         <p className="mb-4">We may update this policy from time to time. The updated version will be indicated by an
             updated “Revised” date.</p>
 
-        <h2 className="text-xl font-bold mb-2">Contact Us</h2>
-        <p className="mb-4">For any questions or concerns regarding this privacy policy, please contact us at
-            contact@masterbots.ai</p>
+        <h2 className="text-xl font-bold mb-2 uppercase">Contact Us</h2>
+        <p className="mb-4">For any questions or concerns regarding this privacy policy, please contact us at 
+            <a className="underline" href="mailto:contact@masterbots.ai"> contact@masterbots.ai</a></p>
 
 
-            <h2 className="text-xl font-bold mb-2">Introduction</h2>
+         <h2 className="text-xl font-bold mb-2 uppercase">Introduction</h2>
 
         <p className="mb-4">Welcome to Masterbots.ai (&quot;we,&quot;, &quot;us,&quot; &quot;our&quot;). By accessing our website, you agree to these Terms of Service
             (&quot;Terms&quot;), our Privacy Policy, and all applicable laws and regulations governing the use of our platform. Our
             website aggregates AI chatbot conversations and publicizes these chats by default. We provide a platform for
             users to interact with various AI chatbots and share their interactions publicly on our site.</p>
 
-        <h2 className="text-xl font-bold mb-2">Acceptance of Terms</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Acceptance of Terms</h2>
         <p className="mb-4">By using our website, you acknowledge that you have read, understood, and agreed to be bound by these Terms. If
             you do not agree with any part of these Terms, you must not use our website.</p>
 
-        <h2 className="text-xl font-bold mb-2">Use License</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Use License</h2>
         <ul className="list-disc pl-6 mb-4">
             <li>We grant you a limited, non-exclusive, non-transferable license to access and use our website in accordance
                 with these Terms.</li>
@@ -87,7 +109,7 @@ export default async function TermPoliciesPage() {
                 a license from us.</li>
         </ul>
 
-        <h2 className="text-xl font-bold mb-2">User Conduct</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">User Conduct</h2>
         <p className="mb-4">You agree not to use our website for any unlawful purpose or in any way that interrupts, damages, or impairs
             the service. You agree not to attempt to gain unauthorized access to our website or any networks, servers, or
             computer systems connected to our website. You are responsible for ensuring that any content you submit or
@@ -95,41 +117,45 @@ export default async function TermPoliciesPage() {
             otherwise disseminate any content that is unlawful, harmful, threatening, abusive, harassing, defamatory,
             vulgar, obscene, or otherwise objectionable.</p>
 
-        <h2 className="text-xl font-bold mb-2">Publicizing of Chats</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Publicizing of Chats</h2>
         <p className="mb-4">By using our service, you understand and agree that all chats you engage in with our AI chatbots may be
             publicized on our website by default. This includes any text, images, or other content you share during these
             interactions. You grant us a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, adapt,
             publish, and display such content for the purpose of operating and providing our service.</p>
 
-        <h2 className="text-xl font-bold mb-2">Intellectual Property Rights</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Intellectual Property Rights</h2>
         <p className="mb-4">The content published on our website, including but not limited to text, graphics, logos, and software, is
             the property of Masterbots.ai or its licensors and is protected by copyright and intellectual property laws. You
             may not use, copy, modify, or distribute any content from our website without the express permission of the
             owner.</p>
 
-        <h2 className="text-xl font-bold mb-2">Disclaimer of Warranties</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Disclaimer of Warranties</h2>
         <p className="mb-4">Our website is provided on an &quot;AS IS&quot; and &quot;AS AVAILABLE&quot; basis without warranties of any kind, either express
             or implied, including but not limited to warranties of merchantability, fitness for a particular purpose, or
             non-infringement.</p>
 
-        <h2 className="text-xl font-bold mb-2">Limitation of Liability</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Limitation of Liability</h2>
         <p className="mb-4">We shall not be liable for any direct, indirect, incidental, special, consequential, or punitive damages
             resulting from your access to or use of our website.</p>
 
-        <h2 className="text-xl font-bold mb-2">Changes to Terms</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Changes to Terms</h2>
         <p className="mb-4">We reserve the right to modify these Terms at any time. We will provide notice of any significant changes by
             updating the date at the top of these Terms and, where appropriate, notification via email or our
             website.</p>
 
-        <h2 className="text-xl font-bold mb-2">Governing Law</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Governing Law</h2>
         <p className="mb-4">These Terms shall be governed by and construed in accordance with the laws of Delaware, without giving effect
             to any principles of conflicts of law.</p>
 
-        <h2 className="text-xl font-bold mb-2">Contact Us</h2>
+        <h2 className="text-xl font-bold mb-2 uppercase">Contact Us</h2>
         <p className="mb-4">For any questions or concerns regarding these Terms, please contact us at contact@masterbots.ai</p>
    
 
         {/* <!-- Additional sections and content can be added similarly --> */}
+
+        <div className="bg-gray-200 dark:bg-[#242424]  dark:border-l-gray-200 border-l-[#3b3a3a]  border-l-4 p-5 mt-10">
+            <p><strong>Last Revised:</strong> June 21st, 2024</p>
+        </div>
     </div>
     </div>
   )
