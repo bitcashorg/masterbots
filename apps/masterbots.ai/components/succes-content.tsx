4b081f11bfaf293231ee29f4f6dafc787bb0e9d9
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-06-24 15:19:35 +0100

    fix: clean up

--- apps/masterbots.ai/components/succes-content.tsx
+++ apps/masterbots.ai/components/succes-content.tsx
@@ -3,7 +3,7 @@ import { usePayment } from "@/lib/hooks/use-payment";
 import Lottie from "lottie-react";
 import Link from "next/link";
 export function SuccessContent() {
-  const { secret } = usePayment()
+  const { paymentIntent } = usePayment()
 
   return (
     <div
@@ -16,7 +16,7 @@ export function SuccessContent() {
         <h2 className="font-medium text-[24px] ">Successfully subscribed to Masterbots Pro!</h2>
         <span>We will send your receipt via email. </span>
         <span>AND </span>
-        <span className="text-[#71717A]"> You can <Link href={`/u/s/subs/${secret}`} className="text-blue-600">view receipt online</Link> </span>
+        <span className="text-[#71717A]"> You can <Link target="_blank" href={`/u/s/subs/${paymentIntent.id}`} className="text-blue-600">view receipt online</Link> </span>
       </div>
 
       <div className='dark:bg-black border mt-5  w-full border-t-black bg-white p-5 flex justify-center items-center space-x-4'>
