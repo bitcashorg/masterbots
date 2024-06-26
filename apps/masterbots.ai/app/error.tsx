5a07bdd575aa8b2aa34f09917bf80a17dc72638d
Bran18 <andreyfdez18@gmail.com>
2024-06-26 12:25:28 -0600

    fix-guards + dom warning

--- apps/masterbots.ai/app/error.tsx
+++ apps/masterbots.ai/app/error.tsx
@@ -10,7 +10,7 @@ export default function Error({
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-80 h-80 md:w-96 md:h-96 relative">
+      <div className="relative w-80 h-80 md:w-96 md:h-96">
         <Image
           src="/images/404.png"
           alt="404 Not Found"
@@ -19,8 +19,8 @@ export default function Error({
           className="w-full h-full"
         />
       </div>
-      <h1 className="text-3xl font-bold  mb-4">Oops! Page Not Found</h1>
-      <p className=" text-center mb-4">{error.message}</p>
+      <h1 className="mb-4 text-3xl font-bold">Oops! Page Not Found</h1>
+      <p className="mb-4 text-center ">{error.message}</p>
       <Link href="/" className='flex items-center space-x-1'>
         <svg
           width="11"
@@ -32,9 +32,9 @@ export default function Error({
           <path
             d="M7.09973 2.15008L3.24979 6.00003L7.09973 9.84998"
             stroke="#FAFAFA"
-            stroke-width="0.962486"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="0.962486"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>{' '}
         <span >Return Home</span>
