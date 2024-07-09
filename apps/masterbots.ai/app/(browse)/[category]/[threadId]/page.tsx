5f8cf668c1a860f64faa85dead7207db14410814
TopETH <supernathanliu@gmail.com>
2024-07-09 15:35:40 -0400

    fix: use original generateMetadata

--- apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
+++ apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from 'next/navigation'
 import { getThread } from '@/services/hasura'
 import { ChatPageProps } from '@/app/c/[category]/[chatbot]/page'
-import { Metadata } from 'next'
-import { generateMetadataFromSEO } from '@/lib/metadata'
+
+export { generateMbMetadata as generateMetadata } from '@/lib/metadata'
 
 export default async function ChatPage({ params }: ChatPageProps) {
   const thread = await getThread({
@@ -10,28 +10,4 @@ export default async function ChatPage({ params }: ChatPageProps) {
     jwt: ''
   })
   redirect(`/b/${thread.chatbot.name}/${thread.threadId}`)
-}
-
-export async function generateMetadata({
-  params
-}: ChatPageProps): Promise<Metadata> {
-  const thread = await getThread({
-    threadId: params.threadId,
-    jwt: ''
-  })
-
-  const firstQuestion =
-    thread.messages.find(m => m.role === 'user')?.content || 'not found'
-  const firstResponse =
-    thread.messages.find(m => m.role === 'assistant')?.content || 'not found'
-
-  const seoData = {
-    title: firstQuestion,
-    description: firstResponse,
-    ogType: 'website',
-    ogImageUrl: '',
-    twitterCard: 'summary'
-  }
-
-  return generateMetadataFromSEO(seoData)
 }
