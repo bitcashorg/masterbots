bdfaef60084de6f4c18c02c29702fc927835c276
Roberto Lucas <andler.dev@gmail.com>
2024-08-06 15:43:39 -0600

    impr(hasura): sql models enum migration

--- apps/hasura/migrations/masterbots/1718416395664_create_table_models_enum/up.sql
+++ apps/hasura/migrations/masterbots/1718416395664_create_table_models_enum/up.sql
@@ -1,20 +1,11 @@
 CREATE TABLE IF NOT EXISTS public.models_enum (
-    name TEXT UNIQUE NOT NULL,
-    value TEXT UNIQUE
+    name TEXT UNIQUE PRIMARY KEY NOT NULL,
+    value TEXT UNIQUE NOT NULL
 );
 
-INSERT INTO public.models_enum (name) VALUES 
-    ('perplexity'), 
-    ('anthropic'), 
-    ('openAi'), 
-    ('wordware')
-ON CONFLICT (name) DO NOTHING;
-
-UPDATE public.models_enum
-SET value = CASE name
-    WHEN 'perplexity' THEN 'llama3_7B'
-    WHEN 'anthropic' THEN 'claude3_haiku'
-    WHEN 'openAi' THEN 'gpt-4o-mini'
-    WHEN 'wordware' THEN 'wordware'
-    ELSE value
-END;
+INSERT INTO public.models_enum (name, value) VALUES 
+    ('perplexity', 'llama3_7B'),
+    ('anthropic', 'claude3_haiku'),
+    ('openAi', 'gpt-4o-mini'),
+    ('wordware', 'wordware')
+ON CONFLICT (name) DO UPDATE SET value = EXCLUDED.value;
