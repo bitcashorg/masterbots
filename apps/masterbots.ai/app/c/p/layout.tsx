c098eeb2fa6cfd6922efa7a40296b9ade297dc6f
Roberto Lucas <andre.rlucas@outlook.com>
2024-08-16 18:26:46 -0600

    fix: typo

--- apps/masterbots.ai/app/c/p/layout.tsx
+++ apps/masterbots.ai/app/c/p/layout.tsx
@@ -1,5 +1,7 @@
 import { ChatLayoutSection } from '@/components/chat/chat-layout-section'
 import FooterCT from '@/components/footer-ct'
+import { appConfig } from 'mb-env'
+import { redirect } from 'next/navigation'
 import NextTopLoader from 'nextjs-toploader'
 
 interface ChatLayoutProps {
@@ -7,6 +9,11 @@ interface ChatLayoutProps {
 }
 
 export default async function ChatLayout({ children }: ChatLayoutProps) {
+  if (!appConfig.devMode) {
+    console.error('Navigation to Pro is disabled. No access to this page')
+    redirect('/')
+  }
+
   return (
     <main className="relative flex flex-col h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
       <NextTopLoader color="#1ED761" initialPosition={0.2} />
