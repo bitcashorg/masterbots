1487487838dcfb00fbe3bbbd1f8720a82b4442a9
Roberto Lucas <andler.dev@gmail.com>
2024-08-21 14:17:44 -0600

    chore(impr): MB seeds update and upgrade

--- apps/hasura/seeds/masterbots/1699692478312_initial_prompt_type_enum_seed.sql
+++
@@ -1,6 +0,0 @@
-SET check_function_bodies = false;
-
-INSERT INTO public.prompt_type_enum (value) VALUES ('instruction')
-ON CONFLICT (value) DO NOTHING;
-INSERT INTO public.prompt_type_enum (value) VALUES ('prompt')
-ON CONFLICT (value) DO NOTHING;
--- apps/hasura/seeds/masterbots/1699692478313_initial_default_complexity_enum_seed.sql
+++
@@ -1,8 +0,0 @@
-SET check_function_bodies = false;
-
-INSERT INTO public.complexity_enum (value) VALUES ('expert')
-ON CONFLICT (value) DO NOTHING;
-INSERT INTO public.complexity_enum (value) VALUES ('adult')
-ON CONFLICT (value) DO NOTHING;
-INSERT INTO public.complexity_enum (value) VALUES ('eli5')
-ON CONFLICT (value) DO NOTHING;
