79eeb6c446a6dfa55177d3d497228c4b60efa189
Nathanael Liu <supernathanliu@gmail.com>
2024-09-02 15:55:18 -0700

    fix(masterbots.ai): adjust arrow direction in thread list (#255)

--- apps/masterbots.ai/components/chat/chat-accordion.tsx
+++ apps/masterbots.ai/components/chat/chat-accordion.tsx
@@ -82,14 +82,11 @@ export const ChatAccordion = ({
 
   return (
     <div className={className || ''} {...props}>
-      <button
-        data-state={open ? 'open' : 'closed'}
-        onClick={() => {
-          if (!disabled) {
-            toggle()
-          }
-        }}
-        className={`flex flex-1 justify-start flex-col relative
+      {!disabled && (
+        <button
+          data-state={open ? 'open' : 'closed'}
+          onClick={toggle}
+          className={`flex flex-1 justify-start flex-col relative
         transition-all ease-in-out duration-200
         border-transparent border
         hover:rounded-t-[8px]
@@ -98,31 +95,34 @@ export const ChatAccordion = ({
             ? 'dark:border-b-mirage border-b-gray-300'
             : 'dark:hover:border-b-mirage hover:border-b-gray-300 [&>div>div>button]:!hidden'
         } ${triggerClass || ''}`}
-      >
-        {children[0]}
-        {!open && children[1]}
-        <ChevronDown
-          {...(handleTrigger
-            ? {
-                onClick: e => {
-                  e.stopPropagation()
-                  handleTrigger()
+        >
+          {children[0]}
+          {!open && children[1]}
+          <ChevronDown
+            {...(handleTrigger
+              ? {
+                  onClick: e => {
+                    e.stopPropagation()
+                    handleTrigger()
+                  }
                 }
-              }
-            : {})}
-          className={`${open ? '' : '-rotate-90'} absolute -right-2 size-4 shrink-0 mr-4 transition-transform duration-200 ${arrowClass || ''} ${disabled ? 'hidden' : ''}`}
-        />
-      </button>
-      <div
-        className={`text-sm transition-all border
-      ${
-        open
-          ? 'animate-accordion-down dark:border-mirage border-gray-300 !border-t-transparent !border-r-transparent'
-          : 'overflow-hidden animate-accordion-up h-0 border-transparent'
-      } ${contentClass || ''}`}
-      >
-        {children[2]}
-      </div>
+              : {})}
+            className={`${open ? '' : '-rotate-90'} absolute -right-2 size-4 shrink-0 mr-4 transition-transform duration-200 ${arrowClass || ''} ${disabled ? 'hidden' : ''}`}
+          />
+        </button>
+      )}
+      {open && (
+        <div
+          className={`text-sm transition-all border
+          ${
+            open
+              ? 'animate-accordion-down dark:border-mirage border-gray-300 !border-t-transparent !border-r-transparent'
+              : 'overflow-hidden animate-accordion-up h-0 border-transparent'
+          } ${contentClass || ''}`}
+        >
+          {children[2]}
+        </div>
+      )}
     </div>
   )
 }
