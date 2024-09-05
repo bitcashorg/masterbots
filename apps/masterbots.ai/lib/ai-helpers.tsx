4447b2b5c44679a8e10f4c0a277286f697f8a08f
Brandon Fernández <31634868+Bran18@users.noreply.github.com>
2024-09-05 12:37:10 -0600

    feat(impr): Vercel AI SDK Update (#256)
    * chore:ai version upt
    
    * chore:ai version upt
    
    * upt-ai delete
    
    * upt-ai versions
    
    * upt-sdk-actions
    
    * upt-complete-sdk-3.3 + dev notes
    
    * upt-@anthropic-ai/sdk + MessageParam
    
    * Delete apps/masterbots.ai/apps/masterbots.ai/package.json
    
    * Delete apps/masterbots.ai/apps/masterbots.ai/package-lock.json
    
    * impr-convertToCoreMessages ternary

--- apps/masterbots.ai/lib/ai-helpers.tsx
+++ apps/masterbots.ai/lib/ai-helpers.tsx
@@ -1,36 +1,36 @@
-import { AIModels } from '@/app/api/chat/models/models'
-import { AiClientType } from '@/lib/types'
-import { MessageParam } from '@anthropic-ai/sdk/resources'
-import { nanoid } from 'nanoid'
-import { ChatCompletionMessageParam } from 'openai/resources'
+import { AIModels } from '@/app/api/chat/models/models';
+import { AiClientType } from '@/lib/types';
+import { MessageParam } from '@anthropic-ai/sdk/resources';
+import {generateId, CoreMessage} from 'ai'; //* Replacing nanoid with generateId
+import { ChatCompletionMessageParam } from 'openai/resources';
 
 export function getModelClientType(model: AIModels) {
   switch (model) {
     case AIModels.GPT4:
     case AIModels.Default:
-      return 'OpenAI'
+      return 'OpenAI';
     case AIModels.Claude3:
-      return 'Anthropic'
+      return 'Anthropic';
     case AIModels.llama3_7b:
     case AIModels.llama3_8b:
-      return 'Perplexity'
+      return 'Perplexity';
     case AIModels.WordWare:
-      return 'WordWare'
+      return 'WordWare';
     default:
-      throw new Error('Unsupported model specified')
+      throw new Error('Unsupported model specified');
   }
 }
 
-//* Creates a normal base payload for the chat message
+//* Generate a payload for a new thread
 export function createPayload(
   json: { id: string },
   messages: { content: string }[],
   completion: any
 ) {
-  const title = messages[0]?.content.substring(0, 100)
-  const id = json.id ?? nanoid()
-  const createdAt = Date.now()
-  const path = `/c/${id}`
+  const title = messages[0]?.content.substring(0, 100);
+  const id = json.id ?? generateId();
+  const createdAt = Date.now();
+  const path = `/c/${id}`;
   return {
     id,
     title,
@@ -44,17 +44,17 @@ export function createPayload(
         role: 'assistant'
       }
     ]
-  }
+  };
 }
 
-//* function ensuring that the payload is correctly formatted for each client type that needs manipulation before sending to the API and if manipulation is not required we retur playload without any modification
+//* Manipulates payloads to match format of the streamText function
 export function setStreamerPayload(
   model: AiClientType,
   payload: ChatCompletionMessageParam[] | MessageParam[]
-) {
+): ChatCompletionMessageParam[] | MessageParam[] {
   switch (model) {
     case 'WordWare':
-      return payload
+      return payload;
     case 'Anthropic':
       return payload.map(
         (message, index) =>
@@ -64,10 +64,19 @@ export function setStreamerPayload(
               : message.role.replace('system', 'assistant'),
             content: message.content
           }) as MessageParam
-      )
+      );
     case 'OpenAI':
     case 'Perplexity':
     default:
-      return payload
+      return payload;
   }
+}
+
+//* Convert an array of messages into the CoreMessage format
+export function convertToCoreMessages(messages: ChatCompletionMessageParam[]): CoreMessage[] {
+  return messages.map(msg => 
+    msg.role.match(/(user|system|assistant)/)
+      ? { role: msg.role as 'user' | 'system' | 'assistant', content: msg.content as string }
+      : (() => { throw new Error(`Unsupported message role: ${msg.role}`) })()
+  )
 }
