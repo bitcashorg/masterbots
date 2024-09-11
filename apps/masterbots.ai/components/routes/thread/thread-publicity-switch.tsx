4747192dcfc7f3f5c4f0bfc64bcb8fa0cf8efc55
sheriffjimoh <sheriffjimoh88@gmail.com>
2024-09-11 17:10:51 +0100

    fix: observations

--- apps/masterbots.ai/components/routes/thread/thread-publicity-switch.tsx
+++ apps/masterbots.ai/components/routes/thread/thread-publicity-switch.tsx
@@ -1,37 +1,24 @@
 import * as Switch from '@radix-ui/react-switch';
-import { useState } from 'react';
-import { Eye, EyeOff } from 'lucide-react'; // Importing the icons from lucide-react
-import { UpdateThreadVisibility } from '@/services/hasura';
-import { useSession } from 'next-auth/react'
+import { Eye, EyeOff } from 'lucide-react'; // Importing the icons from lucide-react'
 import { cn } from '@/lib/utils';
+import { useThreadVisibility } from '@/lib/hooks/use-thread-visibility';
 
 export  function ThreadPublicitySwitch({ threadId }: { threadId: string }) {   
-  const [isChecked, setIsChecked] = useState(false);
-  const session = useSession()
-  const jwt = session?.data?.user?.hasuraJwt
-
-  const handleSwitchChange = async (checked: boolean) => {
-    setIsChecked(checked);
-    try {
-      await UpdateThreadVisibility({ isPublic: !checked, threadId, jwt });
-    } catch (error) {
-      setIsChecked(!checked);
-      console.error('Failed to update thread visibility:', error);
-    }
-  };
-
+  const { isPublic, toggleVisibility } = useThreadVisibility(threadId);
+ 
   return (
     <Switch.Root
       className="relative inline-flex items-center h-[24px] w-[48px] bg-gray-300 dark:bg-gray-600 rounded-full"
-      checked={isChecked}
-      onCheckedChange={handleSwitchChange}
+      checked={isPublic}
+      onCheckedChange={toggleVisibility}
     >
       <Switch.Thumb
-        className={`w-[24px] h-[24px] bg-gray-100 dark:bg-gray-800 rounded-full transform transition-transform duration-100 ease-in-out ${isChecked ? 'translate-x-[24px]' : ''}`}
+      title={isPublic ? 'Make public' : 'Make Private'}
+       className={cn('w-[30px] h-[30px] bg-gray-100 dark:bg-gray-800 rounded-full transform transition-transform duration-100 ease-in-out', isPublic ? 'translate-x-[24px]' : '')}
       />
       <div className="absolute left-0 right-0 flex justify-between items-center w-full h-full px-[4px]">
-        <Eye className={cn('w-[16px] h-[16px] transition-opacity duration-300', isChecked ? 'text-gray-400' : 'text-gray-200')} />
-        <EyeOff className={cn('w-[16px] h-[16px]  transition-opacity duration-300', isChecked ? 'text-gray-200' : 'text-gray-400')} />
+        <Eye className={cn('w-[18px] h-[18px] transition-opacity duration-300', isPublic ? 'text-gray-400' : 'text-gray-200')} />
+        <EyeOff className={cn('w-[18px] h-[18px]  transition-opacity duration-300', !isPublic ? 'text-gray-400' : 'text-gray-200')} />
       </div>
     </Switch.Root>
   );
