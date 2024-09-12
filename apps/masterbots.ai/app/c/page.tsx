3e9aa34c51f476e203ee949a937c0a6a61d737d7
Roberto Lucas <andler.dev@gmail.com>
2024-09-12 15:53:56 -0600

    wip: seo data impr

--- apps/masterbots.ai/app/c/page.tsx
+++ apps/masterbots.ai/app/c/page.tsx
@@ -1,3 +1,4 @@
+import { authOptions } from '@/auth'
 import ChatThreadListPanel from '@/components/routes/chat/chat-thread-list-panel'
 import ThreadPanel from '@/components/routes/thread/thread-panel'
 import { generateMetadataFromSEO } from '@/lib/metadata'
@@ -6,7 +7,6 @@ import { isTokenExpired } from 'mb-lib'
 import { Metadata } from 'next'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import { authOptions } from '@/auth'
 
 export default async function IndexPage() {
   const session = await getServerSession(authOptions)
@@ -34,7 +34,7 @@ export default async function IndexPage() {
 export async function generateMetadata(): Promise<Metadata> {
   const seoData = {
     title: 'Chat page',
-    description: 'Chat page',
+    description: 'Welcome to the chatbot page. Interact with our AI-powered chatbot and get answers to your questions.',
     ogType: 'website',
     ogImageUrl: '',
     twitterCard: 'summary'
