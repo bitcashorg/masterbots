4940a30116e7f6be744a9a07a1e4fd9d6cff3436
Roberto Lucas <andler.dev@gmail.com>
2024-09-15 15:31:20 -0600

    chore: upt signup default profile pic

--- apps/masterbots.ai/components/routes/chat/chat-layout-section.tsx
+++ apps/masterbots.ai/components/routes/chat/chat-layout-section.tsx
@@ -2,23 +2,32 @@
 
 import { ThreadPopup } from '@/components/routes/thread/thread-popup'
 import { useThread } from '@/lib/hooks/use-thread'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 import * as React from 'react'
 
 export function ChatLayoutSection({ children }: { children: React.ReactNode }) {
-  const { sectionRef, isOpenPopup } = useThread()
+  const { sectionRef, isOpenPopup } = useThread()c
+  const path = usePathname()
+  const isBrowse = !path.includes('/c')
 
   return (
     <section
       ref={sectionRef as React.Ref<HTMLDivElement>}
-      className="h-[calc(100vh-4rem)] group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out lg:ml-[250px] xl:ml-[300px] relative"
+      className={cn(
+        'h-[calc(100vh-4rem)] group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out relative',
+        { 'lg:ml-[250px] xl:ml-[300px]': !isBrowse },
+        //  TODO: Check CSS here
+        { 'lg:ml-[250px] xl:pl-[300px]': isBrowse },
+      )}
     >
       <div
         className="flex flex-col items-center lg:w-[calc(100%-250px)] xl:w-[calc(100%-300px)]"
       >
         {children}
-        {isOpenPopup ? <ThreadPopup /> : ''}
       </div>
 
+      {isOpenPopup ? <ThreadPopup /> : ''}
     </section>
   )
 }
