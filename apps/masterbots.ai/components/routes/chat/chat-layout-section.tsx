9bb504fe30cd2c4ef31ab455f29dd918874ce0ba
Roberto Lucas <andler.dev@gmail.com>
2024-09-15 20:55:13 -0600

    style: clean up

--- apps/masterbots.ai/components/routes/chat/chat-layout-section.tsx
+++ apps/masterbots.ai/components/routes/chat/chat-layout-section.tsx
@@ -3,22 +3,16 @@
 import { ThreadPopup } from '@/components/routes/thread/thread-popup'
 import { useThread } from '@/lib/hooks/use-thread'
 import { cn } from '@/lib/utils'
-import { usePathname } from 'next/navigation'
 import * as React from 'react'
 
 export function ChatLayoutSection({ children }: { children: React.ReactNode }) {
   const { sectionRef, isOpenPopup } = useThread()
-  const path = usePathname()
-  const isBrowse = !path.includes('/c')
 
   return (
     <section
       ref={sectionRef as React.Ref<HTMLDivElement>}
       className={cn(
-        'flex h-[calc(100vh-4rem)] group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out relative lg:w-[calc(100%-250px)] xl:w-[calc(100%-300px)] px-4',
-        { 'lg:ml-[250px] xl:ml-[300px]': !isBrowse },
-        //  TODO: Check CSS here
-        { 'lg:ml-[250px] xl:ml-[300px]': isBrowse },
+        'flex h-[calc(100vh-4rem)] group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out relative lg:w-[calc(100%-250px)] xl:w-[calc(100%-300px)] px-4 md:px-10 lg:ml-[250px] xl:ml-[300px]',
       )}
     >
       <div className="flex flex-col pt-5 w-full gap-10 max-w-screen-lg mx-auto">
