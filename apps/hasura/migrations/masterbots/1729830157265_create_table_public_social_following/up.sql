174424359d17c1b2f6f0157248a573e836b45905
mledezma <marcoledezmacordero09@gmail.com>
2024-10-24 23:59:28 -0600

    impr(db): avoid self follow.

--- apps/hasura/migrations/masterbots/1729830157265_create_table_public_social_following/up.sql
+++ apps/hasura/migrations/masterbots/1729830157265_create_table_public_social_following/up.sql
@@ -7,7 +7,8 @@ CREATE TABLE "public"."social_following" (
   FOREIGN KEY ("follower_id") REFERENCES "public"."user"("user_id") ON UPDATE restrict ON DELETE restrict,
   FOREIGN KEY ("followee_id") REFERENCES "public"."user"("user_id") ON UPDATE restrict ON DELETE restrict,
   UNIQUE ("id"),
-  UNIQUE ("follower_id", "followee_id")
+  UNIQUE ("follower_id", "followee_id"),
+  CHECK ("follower_id" != "followee_id")
 );COMMENT ON TABLE "public"."social_following" IS E'This junction table records social following relationships between users. Each record represents a follower-followee relationship.';
 CREATE INDEX idx_social_following_follower ON "public"."social_following" ("follower_id");
 CREATE INDEX idx_social_following_followee ON "public"."social_following" ("followee_id");
--- apps/hasura/migrations/masterbots/1729833778509_alter_table_public_social_following_drop_column_id/down.sql
+++ apps/hasura/migrations/masterbots/1729833778509_alter_table_public_social_following_drop_column_id/down.sql
@@ -1,4 +1,9 @@
-comment on column "public"."social_following"."id" is E'This junction table records social following relationships between users. Each record represents a follower-followee relationship.';
+-- First create the column
+alter table "public"."social_following" add column "id" uuid;
+
+-- Then set its properties
 alter table "public"."social_following" alter column "id" set default uuid_generate_v4();
 alter table "public"."social_following" alter column "id" drop not null;
-alter table "public"."social_following" add column "id" uuid;
+
+-- Finally add the comment
+comment on column "public"."social_following"."id" is E'This junction table records social following relationships between users. Each record represents a follower-followee relationship.';
