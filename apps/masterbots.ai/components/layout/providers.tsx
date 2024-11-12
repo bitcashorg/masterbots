4058ca9f074129593ad9c62d44ba94c1ac5cdfd1
Brandon Fernández <31634868+Bran18@users.noreply.github.com>
2024-11-12 17:29:21 -0600

    [masterbots.ai] fix: restore desktop navigation link - browse section (#303)
    * fix:restore desktop navigation link - browse
    
    * fix:formating
    
    * feat:yellow combobox btn (JUN-REQUEST)
    
    * glowing effect variant
    
    * upt:removed /b navigation as original v
    
    * feat:powerup mode + provider

--- apps/masterbots.ai/components/layout/providers.tsx
+++ apps/masterbots.ai/components/layout/providers.tsx
@@ -6,9 +6,10 @@ import { SidebarProvider } from '@/lib/hooks/use-sidebar'
 import { ThreadProvider } from '@/lib/hooks/use-thread'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
+import type { ThemeProviderProps } from 'next-themes/dist/types'
 import { ModelProvider } from '@/lib/hooks/use-model'
 import { ThreadVisibilityProvider } from '@/lib/hooks/use-thread-visibility'
+import { PowerUpProvider } from '@/lib/hooks/use-power-up'
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
@@ -18,9 +19,13 @@ export function Providers({ children, ...props }: ThemeProviderProps) {
           <SidebarProvider>
             <TooltipProvider>
               <SessionProvider>
-                <ThreadProvider> 
-                  <ThreadVisibilityProvider> {children}</ThreadVisibilityProvider>
-                </ThreadProvider>
+                <PowerUpProvider>
+                  <ThreadProvider>
+                    <ThreadVisibilityProvider>
+                      {children}
+                    </ThreadVisibilityProvider>
+                  </ThreadProvider>
+                </PowerUpProvider>
               </SessionProvider>
             </TooltipProvider>
           </SidebarProvider>
