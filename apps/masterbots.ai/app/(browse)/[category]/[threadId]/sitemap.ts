cf7fd91fe8d5eb2e2e8c0d54ab7bf856f2fa5785
Luis Carrión <41096968+luighis@users.noreply.github.com>
2024-11-13 04:26:16 -0300

    [masterbots.ai] impr: seo sitemap (#306)
    * chore: sitemap creation
    
    * chore: add description based on category
    
    * chore: update queries in services
    
    * chore: truncate text
    
    * impr: upt (browse)/[category]/[threadId]/sitemap.ts
    
    coderabbitai code suggestion.
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Roberto Lucas <andler@bitcash.org>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/masterbots.ai/app/(browse)/[category]/[threadId]/sitemap.ts
+++ apps/masterbots.ai/app/(browse)/[category]/[threadId]/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const threads = await getThreadsWithoutJWT()
 
   return threads.map(thread => ({
-    url: `${process.env.VERCEL_URL}/${toSlug(thread.chatbot.categories[0].category.name)}/${thread.threadId}`,
-    lastModified: thread.updatedAt
+    url: `${process.env.VERCEL_URL}/${toSlug(thread?.chatbot?.categories?.[0]?.category?.name)}/${thread?.threadId}`,
+    lastModified: thread?.updatedAt ?? new Date().toISOString()
   }))
 }
