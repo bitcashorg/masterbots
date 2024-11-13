4e25b9a8575d4a664c4891a454cc1f8915d68063
Luis Carrión <louiss862@gmail.com>
2024-11-13 02:06:59 -0300

    chore: sitemap creation

--- apps/masterbots.ai/app/c/[category]/[chatbot]/sitemap.ts
+++ apps/masterbots.ai/app/c/[category]/[chatbot]/sitemap.ts
@@ -5,7 +5,7 @@ import { toSlug } from 'mb-lib'
 import type { MetadataRoute } from 'next'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const chatbots = await getChatbots({})
+  const chatbots = await getChatbots({ limit: botNames.size })
 
   return chatbots.map(chatbot => ({
     url: `${process.env.VERCEL_URL}/c/${toSlug(chatbot.categories[0].category.name)}/${getKeyByValue(botNames, chatbot.name)}`,
