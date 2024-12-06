0bea1b576428947246fc7426f1083af33cc8fd34
Brandon Fernández <31634868+Bran18@users.noreply.github.com>
2024-12-06 03:48:12 -0600

    [masterbots.ai] feat: integrate drizzle ORM (#320)
    * feat: create mb-drizzle package initial structure
    
    * feat:(masterbots.ai) add drizzle endpoint + service
    
    * chore: include database url
    
    * chore: upt mb-drizzle
    
    * feat: refactor drizzle config + generations
    
    * chore: impr migrate
    
    * chore: add working drizzle connection + migrations
    
    * feat: add centralized actions
    
    * chore: add ai review recomendations

--- packages/mb-env/src/config.env.ts
+++ packages/mb-env/src/config.env.ts
@@ -9,4 +9,7 @@ export const appConfig = {
     url: `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
     upload_preset: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PERESET || ''
   },
+  database: {
+    url: process.env.DATABASE_URL || '',
+  },
 }
