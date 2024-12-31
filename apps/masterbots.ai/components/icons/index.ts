fa7aacb21e0b3ce33f6c0cd8792441d64211b105
Luis Carrión <louiss862@gmail.com>
2024-12-31 01:43:56 -0300

    chore:remove icons and use lucide icons

--- apps/masterbots.ai/components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as SuccessIcon } from './successIcon'
-export { default as InfoIcon } from './infoIcon'
-export { default as ErrorIcon } from './errorIcon'
-export { default as CloseIcon } from './closeIcon'
