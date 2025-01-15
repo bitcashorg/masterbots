6bf687c1fdc94a45a4d9984020d92e71d215945b
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-01-15 18:31:28 +0100

    fix: update url

--- apps/masterbots.ai/components/layout/header/user-menu.tsx
+++ apps/masterbots.ai/components/layout/header/user-menu.tsx
@@ -15,6 +15,7 @@ import Link from 'next/link'
 import { toSlugWithUnderScore } from 'mb-lib'
 import { ThemeToggle } from '@/components/shared/theme-toggle'
 import { useTheme } from 'next-themes'
+import { urlBuilders } from '@/lib/url'
 
 
 export interface UserMenuProps {
@@ -59,7 +60,7 @@ export function UserMenu({ user }: UserMenuProps) {
         </DropdownMenuTrigger>
         <DropdownMenuContent sideOffset={8} align="start" className="w-[180px]">
           <DropdownMenuItem className="flex-col items-start">
-            <Link href={`/u/${user?.slug ? user?.slug : toSlugWithUnderScore(user?.name || '')}/t`}
+            <Link href={urlBuilders.userProfileUrl({ userSlug: user?.slug ? user.slug : toSlugWithUnderScore(user?.name || '') })}
               className="text-xs"
             >
               <div className="text-xs font-medium">{user?.name}</div>
