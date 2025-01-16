da07b5e346473650eaf3a16f3a032fdafbab179c
Marco Ledezma <marcoledezmacordero09@gmail.com>
2025-01-15 22:44:46 -0600

    Update apps/hasura/migrations/masterbots/1736934906237_set_fk_public_thread_parent_thread/down.sql
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/hasura/migrations/masterbots/1736934906237_set_fk_public_thread_parent_thread/down.sql
+++ apps/hasura/migrations/masterbots/1736934906237_set_fk_public_thread_parent_thread/down.sql
@@ -2,4 +2,4 @@ alter table "public"."thread" drop constraint "thread_parent_thread_fkey",
   add constraint "thread_parent_thread_fkey"
   foreign key ("parent_thread")
   references "public"."thread"
-  ("thread_id") on update restrict on delete restrict;
+  ("thread_id") on update no action on delete no action;
