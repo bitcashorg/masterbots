3d0db8d29ac9bae84962e707b32307571abf4475
Roberto Lucas <andler.dev@gmail.com>
2025-01-30 20:55:44 -0600

    chore: upt icl init seed pt3

--- apps/hasura/seeds/masterbots/1738186795715_init_chatbot_metadata_seeds.sql
+++ apps/hasura/seeds/masterbots/1738186795715_init_chatbot_metadata_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df091b68f75e0e6dff16c5072a9292e49d4bb8169478391eb768e8fabfcf7361
-size 6987
+oid sha256:55c1b439004ee9c12c7f8af7fbc37493bd166491360b84b31f08dd43fbce0709
+size 7596
