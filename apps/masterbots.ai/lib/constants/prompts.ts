419ee9c3df237f2a4500fbfa236603178ed4be85
Roberto Lucas <andler@bitcash.org>
2025-02-05 16:52:39 -0600

    impr: icl metadata requests v1 (#365)
    * wip: icl fetch + meta prompt impr
    
    * impr: sidebar paddings
    
    * wip: icl fetch + meta prompt impr
    
    * wip: icl fetch + meta prompt impr

--- apps/masterbots.ai/lib/constants/prompts.ts
+++ apps/masterbots.ai/lib/constants/prompts.ts
@@ -39,16 +39,31 @@ export function createChatbotMetadataPrompt(
   chatbotMetadata: ChatbotMetadata,
   userPrompt: string,
 ): string {
+  const categories = Object.keys(chatbotMetadata.categories)
+  const tags = chatbotMetadata.tags
   return (
-    `You are a top data scientist with extensive knowledge in the field of ${chatbotMetadata.domainName}. Your sole purpose is to label the following question "${userPrompt}" with the appropriate categories, sub-categories and tags as an array of strings.` +
-    'These are the available categories and their sub-categories: ' +
-    JSON.stringify(chatbotMetadata.categories) +
-    'These are the available tags: ' +
-    chatbotMetadata.tags +
-    `**Important Guidelines:**
-    ` +
-    '- Output only the requested fields without any additional explanation. ' +
-    '- Provide the labels in the exact format as requested.'
+    '<expertise>\n' +
+    `You are an expert in the field of ${chatbotMetadata.domainName}. Your task is to identify the most relevant categories, sub-categories, and tags for the following user question: "${userPrompt}".` +
+    '\n</expertise>\n' +
+    '\n<context>\n' +
+    '\n## Categories and their sub-categories:\n' +
+    categories
+      .map(
+        (category, index) =>
+          `${index + 1}. ${category}:
+      - Sub-categories: ${(chatbotMetadata.categories[category as keyof typeof chatbotMetadata.categories] as unknown as string[]).join(', ')}.`,
+      )
+      .join('\n') +
+    '\n\n## Tags:\n- ' +
+    tags.join('. - ') +
+    '.\n</context>\n' +
+    '\n<instruction>\n' +
+    '- Ensure the selected categories, sub-categories, and tags are highly relevant to the user question.\n' +
+    '- Provide the labels and values in the exact format as requested.\n' +
+    '- Keep the values concise and relevant to the question.\n' +
+    '\n</instruction>\n'
+    // `\n\n**Example:**\n` +
+    // `{categories: ["${categories[0]}", "${categories[1]}", "${categories[2]}"], subCategories: [${(chatbotMetadata.categories[categories[0]] as unknown as string[]).map((sc) => `"${sc}"`).join(', ')}], tags: ["${tags[0]}", "${tags[1]}", "${tags[2]}"]}`
   )
 }
 
