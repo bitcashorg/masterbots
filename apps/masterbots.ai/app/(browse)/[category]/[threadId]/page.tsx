af00c5ecb139b8fafc577c59e3a0db21449408ae
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-02-18 12:28:42 +0100

    update

--- apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
+++ apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
@@ -1,13 +1,18 @@
 import { redirect } from 'next/navigation'
-import { getThread } from '@/services/hasura'
+import { getThreads } from '@/services/hasura'
 import type { ChatPageProps } from '@/types/types'
 
 export { generateMbMetadata as generateMetadata } from '@/lib/metadata'
 
 export default async function ChatPage({ params }: ChatPageProps) {
-  const thread = await getThread({
-    threadId: params.threadId,
-    jwt: ''
-  })
-  redirect(`/b/${thread.chatbot.name}/${thread.threadId}`)
+  // const thread = await getThreads({
+  //   chatbotName: params.threadId,
+  //   jwt: '',
+  //   userId: '',
+  // })
+  // console.log({
+  //   params,
+  //   thread,
+  // })
+  redirect(`/b/${params.threadId}}`)
 }
