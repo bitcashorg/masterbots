aeed57ff85dbf1b986c9ce9b7945d3ec65893312
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-02-19 14:30:18 +0100

    fix: rm double border on chat popup

--- apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
+++ apps/masterbots.ai/app/(browse)/[category]/[threadId]/page.tsx
@@ -5,14 +5,6 @@ import { redirect } from 'next/navigation'
 export { generateMbMetadata as generateMetadata } from '@/lib/metadata'
 
 export default async function ChatPage({ params }: ChatPageProps) {
-  // const thread = await getThreads({
-  //   chatbotName: params.threadId,
-  //   jwt: '',
-  //   userId: '',
-  // })
-  // console.log({
-  //   params,
-  //   thread,
-  // })
-  redirect(`/b/${params.threadId}}`)
+
+  redirect(`/b/${params.threadId.trim()}`)
 }
