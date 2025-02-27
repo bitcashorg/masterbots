f397f673da2f4ae0c5c82e780f523e4381da2040
Roberto Lucas <andler.dev@gmail.com>
2025-02-26 19:05:17 -0600

    chore(wip): upt current chatbots descriptions

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5708c30d8f7dce19b649e9468a822077dc619739b7d256787b5bf4399c723bd1
-size 149987
+oid sha256:a8cd42b5d2457b0e8aaca25add89ca379e91ff0f010d225d123adf366b496629
+size 149628
