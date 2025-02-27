8b415dca280895127e4ce0b00b0bacc8e5f3d03e
Roberto Lucas <andler.dev@gmail.com>
2025-02-27 14:43:28 -0600

    chore: impr init config seeds

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a93c3da6bbbc88e86d2d1feba25a8833c324070f7d5278d68d97b722bf36941
-size 183833
+oid sha256:2f1fedc7ba8d9f06f686fcb418d143b17fa15fbe2e797e1feb5b30db5d6549b2
+size 181607
