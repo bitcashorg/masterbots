aaff93e26175179ef3f576955461176d01bdb116
Roberto Lucas <andler.dev@gmail.com>
2025-04-15 16:41:12 -0600

    feat: isContinued + model type in message table

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f1fedc7ba8d9f06f686fcb418d143b17fa15fbe2e797e1feb5b30db5d6549b2
-size 181607
+oid sha256:181177c243d857a659482a19a3fe3cad2fcbf28d7b9eb5247c02bfc1bca0fbd2
+size 182041
