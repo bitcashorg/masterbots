bc2af7b7193b96330bf7ba49caa16223e9b4db2d
bran18 <andreyfdez18@gmail.com>
2025-04-23 12:01:44 -0600

    impr: continuation logic flow

--- apps/masterbots.ai/components/routes/chat/chat-panel.tsx
+++ apps/masterbots.ai/components/routes/chat/chat-panel.tsx
@@ -76,6 +76,7 @@ export function ChatPanel({
 
 		//? funtion call for continue generation flow
 		continueGeneration()
+		//? reseting the states
 		setIsCutOff(false)
 		setIsContinuing(false)
 	}
