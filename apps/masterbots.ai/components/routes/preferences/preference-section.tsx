b8077b7a273833a9faf761366ce755d9a69e3356
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-04-29 11:02:15 +0100

    feat: added dialog to delete button

--- apps/masterbots.ai/components/routes/preferences/preference-section.tsx
+++ apps/masterbots.ai/components/routes/preferences/preference-section.tsx
@@ -1,10 +1,19 @@
-'use client'
 import {
 	Accordion,
 	AccordionContent,
 	AccordionItem,
 	AccordionTrigger,
 } from '@/components/ui/accordion'
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { Switch } from '@/components/ui/switch'
@@ -14,91 +23,131 @@ import type { PreferenceSectionProps } from '@/types/types'
 import { AArrowDown, AArrowUp, Plus } from 'lucide-react'
 import { PreferenceItemTitle } from './preference-item'
 
+import { IconSpinner } from '@/components/ui/icons'
+import { useState } from 'react'
+
 export function PreferenceSection({
 	title,
 	items,
 	variant,
 }: PreferenceSectionProps) {
+	const [deleteDialogOpen, setDeleteDialogOpen] = useState(false)
+	const [isRemovePending, setIsRemovePending] = useState(false)
+	const [buttonType, setButtonType] = useState('')
+
 	function executeButton(buttonText: string) {
-		return () => {
-			alert(`Button ${buttonText} clicked!`)
-		}
+		setDeleteDialogOpen(true)
+		setButtonType(buttonText)
+	}
+
+	function handleDelete() {
+		// Handle delete action here
+		alert('Delete action executed!')
+		alert(buttonType)
+		setDeleteDialogOpen(false)
 	}
 	return (
-		<Accordion key={title} type="single" collapsible defaultValue="1">
-			<AccordionItem value="1" className="border-none">
-				<AccordionTrigger className="hover:no-underline">
-					<p className="text-2xl">{title}</p>
-				</AccordionTrigger>
-				<AccordionContent>
-					<Card className="bg-transparent border-mirage">
-						<CardContent className="px-4 py-8 flex flex-col justify-center items-center gap-y-4 w-full">
-							{items.map((item, idx) => (
-								<div
-									key={item.title}
-									className={cn(
-										'flex justify-between items-center gap-x-5 px-4 border-b border-mirage pb-5 w-full',
-										idx === items.length - 1 ? 'border-none' : '',
-									)}
-								>
-									<PreferenceItemTitle
-										title={item.title}
-										description={item.description}
-									/>
-									{item.type === 'switch' && (
-										<Switch
-											defaultChecked={
-												'defaultChecked' in item ? item.defaultChecked : false
-											}
-											className="data-[state=unchecked]:bg-mirage data-[state=checked]:bg-mirage [&>span[data-state=unchecked]]:bg-[#71717A] [&>span[data-state=checked]]:bg-accent"
+		<>
+			<AlertDialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+						<AlertDialogDescription>
+							{buttonType === 'Delete Account'
+								? 'This will permanently delete  all your personal data and all your conversations and threads from our servers.'
+								: buttonType === 'Delete Threads'
+									? 'This will permanently delete all your published threads and all your related content of your public threads from our servers.'
+									: 'This will permanently delete your chat message and remove yourdata from our servers.'}
+						</AlertDialogDescription>
+					</AlertDialogHeader>
+					<AlertDialogFooter>
+						<AlertDialogCancel>Cancel</AlertDialogCancel>
+						<AlertDialogAction
+							disabled={isRemovePending}
+							onClick={handleDelete}
+						>
+							{isRemovePending && <IconSpinner className="mr-2 animate-spin" />}
+							Delete
+						</AlertDialogAction>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
+
+			<Accordion key={title} type="single" collapsible defaultValue="1">
+				<AccordionItem value="1" className="border-none">
+					<AccordionTrigger className="hover:no-underline">
+						<p className="text-2xl">{title}</p>
+					</AccordionTrigger>
+					<AccordionContent>
+						<Card className="bg-transparent border-mirage">
+							<CardContent className="px-4 py-8 flex flex-col justify-center items-center gap-y-4 w-full">
+								{items.map((item, idx) => (
+									<div
+										key={item.title}
+										className={cn(
+											'flex justify-between items-center gap-x-5 px-4 border-b border-mirage pb-5 w-full',
+											idx === items.length - 1 ? 'border-none' : '',
+										)}
+									>
+										<PreferenceItemTitle
+											title={item.title}
+											description={item.description}
 										/>
-									)}
-									{item.type === 'toggleGroup' && (
-										<ToggleGroup
-											type="single"
-											defaultValue="b"
-											className="gap-0 border rounded-full border-mirage h-7"
-										>
-											<ToggleGroupItem
-												value="a"
-												className="h-full px-2 hover:rounded-l-full data-[state=on]:rounded-l-full data-[state=on]:text-white data-[state=on]:bg-accent"
+										{item.type === 'switch' && (
+											<Switch
+												defaultChecked={
+													'defaultChecked' in item ? item.defaultChecked : false
+												}
+												className="data-[state=unchecked]:bg-mirage data-[state=checked]:bg-mirage [&>span[data-state=unchecked]]:bg-[#71717A] [&>span[data-state=checked]]:bg-accent"
+											/>
+										)}
+										{item.type === 'toggleGroup' && (
+											<ToggleGroup
+												type="single"
+												defaultValue="b"
+												className="gap-0 border rounded-full border-mirage h-7"
 											>
-												<AArrowDown />
-											</ToggleGroupItem>
-											<ToggleGroupItem
-												value="b"
-												className="h-full px-2 border-x border-gray-700 text-base font-medium text-white data-[state=on]:text-white data-[state=on]:bg-accent"
+												<ToggleGroupItem
+													value="a"
+													className="h-full px-2 hover:rounded-l-full data-[state=on]:rounded-l-full data-[state=on]:text-white data-[state=on]:bg-accent"
+												>
+													<AArrowDown />
+												</ToggleGroupItem>
+												<ToggleGroupItem
+													value="b"
+													className="h-full px-2 border-x border-gray-700 text-base font-medium text-white data-[state=on]:text-white data-[state=on]:bg-accent"
+												>
+													MD
+												</ToggleGroupItem>
+												<ToggleGroupItem
+													value="c"
+													className="h-full px-2 hover:rounded-r-full data-[state=on]:rounded-r-full data-[state=on]:text-white data-[state=on]:bg-accent"
+												>
+													<AArrowUp />
+												</ToggleGroupItem>
+											</ToggleGroup>
+										)}
+										{item.type === 'button' && (
+											<Plus className="cursor-pointer" />
+										)}
+										{item.type === 'dangerButton' && (
+											<Button
+												onClick={() => executeButton(item.buttonText ?? '')}
+												className="bg-transparent border border-destructive text-destructive p-2 text-sm min-h-9"
 											>
-												MD
-											</ToggleGroupItem>
-											<ToggleGroupItem
-												value="c"
-												className="h-full px-2 hover:rounded-r-full data-[state=on]:rounded-r-full data-[state=on]:text-white data-[state=on]:bg-accent"
-											>
-												<AArrowUp />
-											</ToggleGroupItem>
-										</ToggleGroup>
-									)}
-									{item.type === 'button' && (
-										<Plus className="cursor-pointer" />
-									)}
-									{item.type === 'dangerButton' && (
-										<Button
-											onClick={() => executeButton(item.buttonText ?? '')}
-											className="bg-transparent border border-destructive text-destructive p-2 text-sm min-h-9"
-										>
-											{'icon' in item && item.icon && (
-												<item.icon className="mr-1 size-4" />
-											)}
-											{'buttonText' in item && item.buttonText}
-										</Button>
-									)}
-								</div>
-							))}
-						</CardContent>
-					</Card>
-				</AccordionContent>
-			</AccordionItem>
-		</Accordion>
+												{'icon' in item && item.icon && (
+													<item.icon className="mr-1 size-4" />
+												)}
+												{'buttonText' in item && item.buttonText}
+											</Button>
+										)}
+									</div>
+								))}
+							</CardContent>
+						</Card>
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
+		</>
 	)
 }
