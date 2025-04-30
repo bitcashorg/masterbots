ecce3ed0e86a4a18564cc402ed9cc5249e8ea2f7
Roberto Lucas <andler.dev@gmail.com>
2025-04-30 02:35:36 -0600

    perf(impr): google sign-in

--- apps/masterbots.ai/components/shared/login-button.tsx
+++ apps/masterbots.ai/components/shared/login-button.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Button, type ButtonProps } from '@/components/ui/button'
+import { IconGoogle } from '@/components/ui/icons'
 import { cn } from '@/lib/utils'
 import { signIn } from 'next-auth/react'
 import * as React from 'react'
@@ -20,11 +21,12 @@ export function LoginButton({
 	const [isLoading, setIsLoading] = React.useState(false)
 	return (
 		<Button
-			variant="outline"
+			variant="secondary"
+			size="xl"
 			onClick={() => {
 				setIsLoading(true)
 				// next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
-				signIn('google', { callbackUrl: '/' })
+				signIn('google', { callbackUrl: '/chat' })
 			}}
 			disabled={isLoading}
 			className={cn(className)}
@@ -35,6 +37,9 @@ export function LoginButton({
       ) : showGithubIcon ? (
         <IconGitHub className="mr-2" />
       ) : null} */}
+			{text === 'Login with Google' && (
+				<IconGoogle className="fill-foreground stroke-foreground size-5" />
+			)}
 			{text}
 		</Button>
 	)
