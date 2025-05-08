9d14b3b52be151f19e7e5315fe5378776b748069
Roberto Lucas <andler.dev@gmail.com>
2025-05-08 02:35:36 -0600

    chore: upt phase 3 init config seeds, adding rel

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a21cac57f04bfd2dc305ea7fc06a0c890e4799f53647742dd8603ef4583d66c4
-size 203349
+oid sha256:2199b390744c2a4145d429c7a23741fb2cac5d1c3e3a6be9b5be20cb90b6ae05
+size 335833
