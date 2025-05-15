e0762dc82fed0aacf1c621a75fdf01d2dd9aeca2
Roberto Lucas <andler.dev@gmail.com>
2025-05-15 00:00:49 -0600

    chore: restore skipped seed lines

--- apps/hasura/seeds/masterbots/1724271272084_init_threads_seeds.sql
+++ apps/hasura/seeds/masterbots/1724271272084_init_threads_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a292ccf609b3b4f499b197e8d566a053f28fb7ec7df89bcfe2ab4b71b862cf8
-size 213055
+oid sha256:997b401bb13349616c6bd2c374727af2243438985d8c1ebe9deafc3664939dd9
+size 210700
--- apps/hasura/seeds/masterbots/1724271332200_init_messages_seeds.sql
+++ apps/hasura/seeds/masterbots/1724271332200_init_messages_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cc5483debe309dddde3e5e886311de3197547aec35cfec608197d3949bb0822
-size 5667713
+oid sha256:4db7a9a17393d483cc52df6bb5d1d88806dda4053ff0409777ab9b2add17c33f
+size 5653589
