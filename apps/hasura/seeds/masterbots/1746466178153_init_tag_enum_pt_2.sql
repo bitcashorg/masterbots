b1545fd1a97ae8aa0be5caaf503ffc03bf637f32
Roberto Lucas <andler@bitcash.org>
2025-05-15 00:04:31 -0600

    chore: hasura, set icl seeds chunks (#484)
    * chore: hasura, set icl seeds chunks
    
    * chore: track backup icl seed loc to lfs
    
    * chore(seeds): uncomment phase 3 topics & add new chatbots + icl metadata rel
    
    * chore: upt phase 3 init config seeds, adding rel
    
    * fix: example seeds integrity
    
    * test: incr hasura max req body
    
    * revert: cloudbuild og config
    
    * chore: restore skipped seed lines

---
+++ apps/hasura/seeds/masterbots/1746466178153_init_tag_enum_pt_2.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:898b310ac14ae7772b22041f8e84e3edbc9027ca4104af0d2e3381b8d81823b3
+size 18030076
---
+++ apps/hasura/seeds/masterbots/1746466541645_init_example_pt_9.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1be7b2d8081eee16af351e2e5969d746b4c4e408800a063fa32eff49fc82455b
+size 23371087
