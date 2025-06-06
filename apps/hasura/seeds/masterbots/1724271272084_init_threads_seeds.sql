fe9dc3aba3bb6cca1b2756e954f69e1658ba778d
Roberto Lucas <andler.dev@gmail.com>
2025-06-05 22:16:21 -0600

    chore: sync thread & msgs seeds

--- apps/hasura/seeds/masterbots/1724271272084_init_threads_seeds.sql
+++ apps/hasura/seeds/masterbots/1724271272084_init_threads_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c53ed85a0304c0e731cd618b5d8240a81e2b18c6e6bda4c62a26c41137b2673
-size 303100
+oid sha256:997b401bb13349616c6bd2c374727af2243438985d8c1ebe9deafc3664939dd9
+size 210700
--- apps/hasura/seeds/masterbots/1724271332200_init_messages_seeds.sql
+++ apps/hasura/seeds/masterbots/1724271332200_init_messages_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad033704442672b202d506e7d33ff1850e99cfc2858ba4e5bf6aa4a35f37a6a1
-size 6189085
+oid sha256:4db7a9a17393d483cc52df6bb5d1d88806dda4053ff0409777ab9b2add17c33f
+size 5653589
