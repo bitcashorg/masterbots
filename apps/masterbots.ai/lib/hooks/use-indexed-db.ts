2fa308d29bd19ef7c2c83e5a7bae9608c02d482a
Roberto Lucas <andler@bitcash.org>
2025-06-06 19:02:49 -0600

    feat: attachment thread metadata (#502)
    * feat(wip): track thread attch data
    
    * chore: upt drizzle & genql schemas
    
    * feat(wip): save user attachments on load chk
    
    * perf: file upload throughout indexed db hook
    
    * feat(wip): gcp bucket setup
    
    * feat: enlarge thread attachment
    
    * fix: attachment upload loop
    
    * fix: gcloud storage + thread metadata check & upload
    
    * fix: bucket upload + after upload mutations
    
    * feat: store remote thread metadata to indexeddb
    
    * fix: read file content instead url
    
    * fix(wip): get attachments on chat onFinish
    
    * fix: attach new stack of files to chat

--- apps/masterbots.ai/lib/hooks/use-indexed-db.ts
+++ apps/masterbots.ai/lib/hooks/use-indexed-db.ts
@@ -1,6 +1,21 @@
+import {
+	getAllUserThreadMetadata,
+	getUserThreadsMetadata,
+	updateThreadMetadata,
+	uploadAttachmentToBucket,
+} from '@/app/actions'
 import type { FileAttachment } from '@/lib/hooks/use-chat-attachments'
+import { useUploadImagesCloudinary } from '@/lib/hooks/use-cloudinary-upload'
+import { logErrorToSentry } from '@/lib/sentry'
+import { prepareThreadAttachmentCheck } from '@/lib/threads'
+import { Attachment } from 'ai'
+import { isEqual, merge, pick, uniqBy } from 'lodash'
+import { message } from 'mb-drizzle'
 import { appConfig } from 'mb-env'
+import { Thread } from 'mb-genql'
+import { fetchJson } from 'mb-lib'
 import { useEffect, useRef, useState } from 'react'
+import { thread } from '../../../../packages/mb-drizzle/src/drizzle/schema'
 
 const DEFAULT_DB_NAME = 'masterbots_attachments_indexed_db'
 const DEFAULT_STORE_NAME = 'masterbots_attachments_store'
@@ -11,7 +26,9 @@ export function useIndexedDB({
 }) {
 	const dbRef = useRef<IDBDatabase | null>(null)
 	const [mounted, setMounted] = useState(false)
-	const db = dbRef.current
+	const [remoteThreadMetadata, setRemoteThreadMetadata] = useState<
+		IndexedDBItem[]
+	>([])
 
 	const onMountSuccess = (event: Event) => {
 		if (appConfig.features.devMode) {
@@ -19,6 +36,9 @@ export function useIndexedDB({
 		}
 		dbRef.current = (event.target as IDBOpenDBRequest).result
 		setMounted(true)
+
+		// check if we have records in the store to update user threads metadata
+		// getAllItems()
 	}
 
 	const onUpgradeNeeded = (event: IDBVersionChangeEvent) => {
@@ -33,6 +53,7 @@ export function useIndexedDB({
 	}
 
 	const resetState = () => {
+		const db = dbRef.current
 		setMounted(false)
 		if (db) {
 			db.close()
@@ -55,6 +76,7 @@ export function useIndexedDB({
 	}, [dbName, storeName])
 
 	const addItem = (item: IndexedDBItem) => {
+		const db = dbRef.current
 		if (!db) return
 		const transaction = db.transaction(storeName, 'readwrite')
 		const store = transaction.objectStore(storeName)
@@ -63,6 +85,7 @@ export function useIndexedDB({
 
 	const getItem = (id: IDBValidKey): Promise<IndexedDBItem> => {
 		return new Promise((resolve, reject) => {
+			const db = dbRef.current
 			if (!db) return reject('Database not initialized')
 			const transaction = db.transaction(storeName, 'readonly')
 			const store = transaction.objectStore(storeName)
@@ -78,16 +101,201 @@ export function useIndexedDB({
 		})
 	}
 
-	const getAllItems = (): Promise<IndexedDBItem[]> => {
-		return new Promise((resolve, reject) => {
+	const getAllItems = async (): Promise<IndexedDBItem[]> => {
+		return await new Promise((resolve, reject) => {
+			const db = dbRef.current
 			if (!db) return reject('Database not initialized')
 			const transaction = db.transaction(storeName, 'readonly')
 
 			const store = transaction.objectStore(storeName)
 			const request = store.getAll()
 
-			request.onsuccess = () => {
-				resolve(request.result)
+			request.onsuccess = async () => {
+				const attachments = request.result as IndexedDBItem[]
+
+				if (appConfig.features.devMode) {
+					console.info('IndexedDB records:', attachments)
+				}
+
+				let newAttachments: FileAttachment[] = attachments as FileAttachment[]
+				const currentUserMetadata = await getAllUserThreadMetadata()
+				const newAttachmentCheck = prepareThreadAttachmentCheck(newAttachments)
+				const currentAttachmentCheck =
+					prepareThreadAttachmentCheck(currentUserMetadata)
+				// console.log('currentAttachmentCheck', currentAttachmentCheck)
+				// console.log('newAttachmentCheck', newAttachmentCheck)
+				// console.log('isEqual(newAttachmentCheck, currentAttachmentCheck)', isEqual(newAttachmentCheck, currentAttachmentCheck))
+				if (isEqual(newAttachmentCheck, currentAttachmentCheck)) {
+					if (appConfig.features.devMode) {
+						console.warn('No update required. Local is sync with remote')
+					}
+					setRemoteThreadMetadata(newAttachments)
+					return resolve(newAttachments)
+				}
+
+				if (
+					currentUserMetadata &&
+					currentUserMetadata.length > newAttachments.length
+				) {
+					// This means that the remote has more attachments than local hence, the user is on a new browser or device
+					if (appConfig.features.devMode) {
+						console.warn(
+							'Remote has more attachments than local, updating local attachments',
+						)
+					}
+
+					const downloadedAttachments: FileAttachment[] = []
+
+					for (const attachment of currentUserMetadata) {
+						const response = await fetch(attachment.url)
+						if (!response.ok)
+							throw new Error(`Failed to fetch ${attachment.name} from GCS`)
+
+						const blob = await response.blob()
+						const base64 = await new Promise<string>((resolve, reject) => {
+							const reader = new FileReader()
+							reader.onloadend = () => resolve(reader.result as string)
+							reader.onerror = reject
+							reader.readAsDataURL(blob) // This includes the MIME type automatically
+						})
+						const downloadedAttachment: FileAttachment = {
+							...attachment,
+							content: base64,
+						}
+
+						try {
+							updateItem(downloadedAttachment.id, downloadedAttachment)
+						} catch (error) {
+							addItem(downloadedAttachment)
+						}
+
+						downloadedAttachments.push(downloadedAttachment)
+					}
+
+					return resolve(downloadedAttachments)
+				}
+
+				for (const attachment of newAttachments as FileAttachment[]) {
+					const thread = await getUserThreadsMetadata(attachment.messageIds)
+
+					if (!thread) {
+						if (appConfig.features.devMode) {
+							console.warn(
+								`No thread found for messageId: ${attachment.messageIds}, skipping attachment update`,
+								attachment.id,
+								attachment.name,
+							)
+						}
+						continue
+					}
+
+					const remoteMetadataAttachments = (thread?.metadata as ThreadMetadata)
+						?.attachments
+					const doesThreadMetadataExist = remoteMetadataAttachments?.some(
+						(att) =>
+							att.id === attachment.id &&
+							att.messageIds.length === attachment.messageIds.length,
+					)
+
+					// If the attachment already exists in the thread metadata, skip updating it
+					if (doesThreadMetadataExist) {
+						if (appConfig.features.devMode) {
+							console.warn(
+								'Attachment already exists in thread metadata, skipping update',
+								attachment.id,
+								attachment.name,
+							)
+						}
+						continue
+					}
+
+					// Ensuring remote would have the latest attachments related messageIds
+					newAttachments = uniqBy(
+						[
+							...((thread.metadata as ThreadMetadata | null)?.attachments ||
+								[]),
+							...newAttachments,
+						],
+						'id',
+					).map((att) => {
+						if (att.id === attachment.id) {
+							return {
+								...att,
+								messageIds: attachment.messageIds,
+							}
+						}
+						return att
+					})
+
+					try {
+						const { data: uploadAttachmentData } = await fetchJson<{
+							data: FileAttachment | null
+							error: string | null
+						}>('/api/attachments/upload', {
+							method: 'POST',
+							body: JSON.stringify({
+								attachment,
+								thread,
+							}),
+							headers: {
+								'Content-Type': 'application/json',
+							},
+						})
+
+						if (!uploadAttachmentData) {
+							throw new Error('Failed to upload attachment, no data returned')
+						}
+						newAttachments = newAttachments.map((att) => {
+							if (att.id === attachment.id) {
+								return {
+									...att,
+									...uploadAttachmentData,
+								}
+							}
+							return att
+						})
+					} catch (error) {
+						console.error(
+							'Failed to upload the attachment to the bucket: ',
+							error,
+						)
+						newAttachments.push(attachment)
+					}
+
+					newAttachments = uniqBy(newAttachments, 'id')
+				}
+
+				const messagesIds = newAttachments.flatMap(
+					(att) => (att as FileAttachment).messageIds,
+				)
+				const metadataUpdateResults = await updateThreadMetadata(messagesIds, {
+					attachments: newAttachments,
+				})
+				console.log('metadataUpdateResults', metadataUpdateResults)
+				setRemoteThreadMetadata(newAttachments)
+
+				// If new attachments are not equal from remote/local
+				if (!isEqual(attachments, newAttachments)) {
+					console.log(
+						'Detected attachments not equal with new attachments hence, either the new attachments or local has to be updates',
+						{
+							newAttachments: {
+								length: newAttachments.length,
+								msgIds: newAttachments.map(
+									(att) => (att.messageIds as string[]).length,
+								),
+							},
+							attachments: {
+								length: attachments.length,
+								msgIds: attachments.map(
+									(att) => (att.messageIds as string[]).length,
+								),
+							},
+						},
+					)
+				}
+
+				return resolve(newAttachments)
 			}
 
 			request.onerror = () => {
@@ -97,6 +305,7 @@ export function useIndexedDB({
 	}
 
 	const updateItem = (id: string, updatedItem: IndexedDBItem) => {
+		const db = dbRef.current
 		if (!db) return
 		const transaction = db.transaction(storeName, 'readwrite')
 		const store = transaction.objectStore(storeName)
@@ -105,6 +314,7 @@ export function useIndexedDB({
 	}
 
 	const deleteItem = (id: number) => {
+		const db = dbRef.current
 		if (!db) return
 		const transaction = db.transaction(storeName, 'readwrite')
 		const store = transaction.objectStore(storeName)
@@ -115,3 +325,4 @@ export function useIndexedDB({
 }
 
 export type IndexedDBItem = FileAttachment | Record<string, unknown>
+export type ThreadMetadata = Record<string, FileAttachment[]>
