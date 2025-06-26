84ed67d5b4510c2627911cc2fe661f3992abd0b9
Roberto Romero Lucas <contact@andler.dev>
2025-06-26 00:06:05 -0600

    feat(wip): phase 4, topics + cat & chatbot order fields

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:452945407cece23080ede533a22c29ebd1a1e763e5de93876804cdfd8aae5db8
-size 335842
+oid sha256:8684c7fe266bd5916d869e668f3df3104e2afa1b09635fbe0fb52c9b8be8a03d
+size 337242
