bbfb8de784b37fda057dcae50b8e919714752f10
Roberto 'Andler' Lucas <contact@andler.dev>
2025-07-10 00:29:21 -0600

    chore(wip): upt init config seed, phase 4, category order upt + wip chatbot new descriptions

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8684c7fe266bd5916d869e668f3df3104e2afa1b09635fbe0fb52c9b8be8a03d
-size 337242
+oid sha256:3c8b2977dbaa714548a772727f70b283eb2e6295c27417f88f68dd4fa80e5c69
+size 366487
