a85a4d84089f70a9b8a82cb613734298ffd81baa
Roberto 'Andler' Lucas <contact@andler.dev>
2025-07-11 13:41:09 -0600

    chore: upt chatbot prompt junction seeds inserts

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b05a71cf856ab8c9d0fa762c1d096eded619b566a2c03cbde6ca33541be2dc0
-size 482453
+oid sha256:32a532cb4ab75a9a5f8a5553583d74870a236306c68141d0f9c6cfdcc180ee78
+size 497271
