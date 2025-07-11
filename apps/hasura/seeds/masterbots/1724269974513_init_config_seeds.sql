cb4bfc620050069947037b586936c6657860fdaf
Roberto 'Andler' Lucas <contact@andler.dev>
2025-07-10 23:36:02 -0600

    chore: upt chatbot new descriptions + new bots and prompts

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c8b2977dbaa714548a772727f70b283eb2e6295c27417f88f68dd4fa80e5c69
-size 366487
+oid sha256:6b05a71cf856ab8c9d0fa762c1d096eded619b566a2c03cbde6ca33541be2dc0
+size 482453
