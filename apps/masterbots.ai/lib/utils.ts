fb2c6fc4200b96216aaf4a32648d13f2916fd0b6
Roberto 'Andler' Lucas <contact@andler.dev>
2025-07-16 16:38:52 -0600

    fix: getRouteType

--- apps/masterbots.ai/lib/utils.ts
+++ apps/masterbots.ai/lib/utils.ts
@@ -317,7 +317,9 @@ export function getRouteType(pathname: string | null): RouteType {
 	if (
 		normalizedPath.startsWith('/c') &&
 		normalizedPath !== '/career' &&
-		!normalizedPath.startsWith('/career/')
+		!normalizedPath.startsWith('/career/') &&
+		normalizedPath !== '/content-creation' &&
+		!normalizedPath.startsWith('/content-creation/')
 	) {
 		return 'chat'
 	}
