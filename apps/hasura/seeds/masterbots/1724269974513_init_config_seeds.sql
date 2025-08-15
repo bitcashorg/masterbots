dadeccedc4fbf6119b8245f79852084a74e8bf07
Roberto 'Andler' Lucas <contact@andler.dev>
2025-08-15 14:36:35 -0600

    chore: upt canonical domains + christbot, biblebot and new bot updated

--- apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
+++ apps/hasura/seeds/masterbots/1724269974513_init_config_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02507f812cd0228406578a9dd9635958989fccd27b4d473d086d3750e697c043
-size 468638
+oid sha256:6d5856a2c4afedbadc7fc4bc94d25fad95c333999bb58a96b8e04b5db4a2231e
+size 472122
