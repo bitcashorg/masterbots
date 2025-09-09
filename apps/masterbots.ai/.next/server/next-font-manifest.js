bea9944f322a267c47bd36cd20419df121c79e85
sheriffjimoh <sheriffjimoh88@gmail.com>
2025-09-09 23:44:34 +0100

    fix: app clean up

--- apps/masterbots.ai/.next/server/next-font-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__NEXT_FONT_MANIFEST =
-  '{"pages":{},"app":{"/Users/sheriff/Desktop/bitcash-apps/masterbots/apps/masterbots.ai/app/layout":["static/media/5b01f339abf2f1a5.p.woff2","static/media/028c0d39d2e8f589-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
